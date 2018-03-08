8ea416487ac9a1dba1895f858c6fde3ef699a56e
ZARDeonBotha <deon.botha@modusbox.com>
2018-03-08 15:42:00 +0200

    Additional fixes needed to NPM start with the Mysql changes

--- migrations/20170321171929_create-accounts.js
+++ migrations/20170321171929_create-accounts.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('accounts', (t) => {
-    t.increments('accountId').primary()
+    t.increments('accountId').notNullable().primary().defaultTo(1)
     t.string('name', 256).notNullable()
     t.timestamp('createdDate').notNullable().defaultTo(knex.fn.now())
     t.boolean('isDisabled').notNullable().defaultTo(false)
