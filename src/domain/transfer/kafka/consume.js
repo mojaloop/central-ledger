3c9c64c2381045636a8d953b06c61804479739f2
Miguel de Barros <miguel@debarros.me>
2018-03-12 14:45:30 +0200

    Reworked consumer to create a single consumer per topic

--- src/domain/transfer/kafka/consume.js
+++ src/domain/transfer/kafka/consume.js
@@ -21,145 +21,109 @@ const Commands = require('../commands')
 
 var CronJob = require('cron').CronJob
 
-const kafkaConsumer = (clientId, funcProcessMessage, topicRegexFilter, options) => {
-  Logger.info(`kafkaConsumer:: Creating Kafka Consumer clientId:'${clientId}', funcProcessMessage:'${funcProcessMessage.name || 'anonymousFunc'}', topicRegexFilter:'${topicRegexFilter}'`)
+const createConsumer = (clientId, funcProcessMessage, topic, options) => {
+  Logger.info(`kafkaConsumer::${clientId} Creating Kafka Consumer funcProcessMessage:'${funcProcessMessage.name || 'anonymousFunc'}''`)
 
-  kafka.getListOfFilteredTopics(topicRegexFilter).then(listOfPreparedTopics => {
-    var templistOfPreparedTopics = listOfPreparedTopics
-    // Logger.info(`List of Topics for for Prepare= ${listOfPreparedTopics}`)
+  var topicList = [{
+    topic: topic,
+    partition: 0
+  }]
 
-    var topicList = templistOfPreparedTopics.map(topic => {
-      return {
-        topic: topic,
-        partition: 0
+  const client = new Client(Config.TOPICS_KAFKA_HOSTS, 'clientId')
+
+  Logger.info(`kafkaConsumer::${clientId} ready for Consumption for ${JSON.stringify(topicList)}`)
+
+  var consumer = new Consumer(
+    client,
+    topicList,
+    options
+  )
+
+  var q = async.queue(function (message, cb) {
+    var payload = JSON.parse(message.value)
+    funcProcessMessage(payload).then(result => {
+      if (result) {
+        // Logger.info('result: %s', result)
+        consumer.commit(
+          function (err, result) {
+            if (err) {
+              Logger.info(`kafkaConsumer::${clientId} Committing index error: ${JSON.stringify(err)}`)
+            }
+            Logger.info(`kafkaConsumer::${clientId} Committing index`)
+          })
+        cb() // this marks the completion of the processing by the worker
+      }
+    }).catch(reason => {
+      Logger.error(`kafkaConsumer::${clientId} funcProcessMessage(${funcProcessMessage.name}) failed with the following reason: ${reason}`)
+    })
+  }, 1)
+
+  // a callback function, invoked when queue is empty.
+  q.drain = function () {
+    consumer.resume()
+  }
+
+  consumer.on('message', (message) => {
+    Logger.info(`kafkaConsumer::${clientId} Consumed message: ${JSON.stringify(message)}`)
+    q.push(message, function (err, result) {
+      if (err) {
+        Logger.error(err)
+        return
       }
     })
+    consumer.pause() // Pause kafka consumer group to not receive any more new messages
+  })
 
-    const client = new Client(Config.TOPICS_KAFKA_HOSTS, 'clientId')
+  consumer.on('error', function (err) {
+    Logger.error(`kafkaConsumer::${clientId} ERROR: ${err}`)
+  })
+}
 
-    Logger.info(`kafkaConsumer:: Client ${clientId} ready for Consumption for ${JSON.stringify(topicList)}`)
+const kafkaConsumers = async (funcProcessMessage, topicRegexFilter, options, config) => {
+  Logger.info(`kafkaConsumer:: Creating Kafka Consumer funcProcessMessage:'${funcProcessMessage.name || 'anonymousFunc'}', topicRegexFilter:'${topicRegexFilter}'`)
 
-    var consumer = new Consumer(
-      client,
-      topicList,
-      options
-    )
+  await kafka.getListOfFilteredTopics(topicRegexFilter).then(listOfPreparedTopics => {
+    var templistOfPreparedTopics = listOfPreparedTopics
+    // Logger.info(`List of Topics for for Prepare= ${listOfPreparedTopics}`)
+    // return new Promise((resolve, reject) => {
+    templistOfPreparedTopics.forEach(topic => {
+      var clientId = 'CONSUMER-' + topic
+      Logger.info(`kafkaConsumer:: Creating Kafka Consumer with ClientId='${clientId}'`)
+      createConsumer(clientId, funcProcessMessage, topic, options)
+    })
 
-    const reLoadConsumersJob = new CronJob('*/10 * * * * *', function () {
-      Logger.info(`kafkaConsumer:: Polling for new topic changes for Consumer: '${clientId}'`)
+    const reLoadConsumersJob = new CronJob(config.pollingCronTab, function () {
+      Logger.info(`kafkaConsumer:: Polling for new topic changes for Consumer`)
       kafka.getListOfFilteredTopics(topicRegexFilter).then(refreshedListOfPreparedTopics => {
-        Logger.info(`kafkaConsumer:: Client ${clientId} ready for Consumption for ${JSON.stringify(refreshedListOfPreparedTopics)}`)
+        Logger.info(`kafkaConsumer:: ready for Consumption for ${JSON.stringify(refreshedListOfPreparedTopics)}`)
         var difference = refreshedListOfPreparedTopics.filter(x => !templistOfPreparedTopics.includes(x))
-        Logger.info(`kafkaConsumer:: Client ${clientId} adding new Topics for Consumption for ${JSON.stringify(difference)}`)
+        Logger.info(`kafkaConsumer:: adding new Topics for Consumption for ${JSON.stringify(difference)}`)
         templistOfPreparedTopics = refreshedListOfPreparedTopics
         if (difference && difference.length) {
-          topicList = difference.map(topic => {
-            return {
-              topic: topic,
-              partition: 0
-            }
+          templistOfPreparedTopics.forEach(topic => {
+            var clientId = 'CONSUMER-' + topic
+            Logger.info(`kafkaConsumer:: Creating Kafka Consumer with ClientId='${clientId}'`)
+            createConsumer(clientId, funcProcessMessage, topic, options)
           })
-          consumer.addTopics(
-            topicList
-            , (err, result) => {
-              if (err) {
-                Logger.error(err)
-              }
-              Logger.info(`kafkaConsumer:: added new Topic result - ${result}`)
-            }, false)
-          // client.refreshMetadata(
-          //   difference
-          //   , (err, result) => {
-          //     if (err) {
-          //       Logger.error(err)
-          //     }
-          //     Logger.info(`kafkaConsumer:: refreshMetadata result - ${result}`)
-          //   })
-          // consumer.pauseTopics(topicList)
-          // consumer.resumeTopics(topicList)
         }
-        // consumer.resumeTopics(topicList)
       })
-    }, null, true, 'America/Los_Angeles')
-
-    var q = async.queue(function (message, cb) {
-      var payload = JSON.parse(message.value)
-      funcProcessMessage(payload).then(result => {
-        if (result) {
-          // Logger.info('result: %s', result)
-          consumer.commit(
-            function (err, result) {
-              if (err) {
-                Logger.info('kafkaConsumer:: Committing index error: %s', (JSON.stringify(err) || JSON.stringify(result)))
-              }
-              Logger.info('kafkaConsumer:: Committing index for %s', message.topic)
-            })
-          cb() // this marks the completion of the processing by the worker
-        }
-      }).catch(reason => {
-        Logger.error(`kafkaConsumer:: funcProcessMessage(${funcProcessMessage.name}) failed with the following reason: ${reason}`)
-      })
-    }, 1)
-
-    // a callback function, invoked when queue is empty.
-    q.drain = function () {
-      consumer.resume() // resume listening new messages from the Kafka consumer group
-    }
-
-    // //on receipt of message from kafka, push the message to local queue, which then will be processed by worker
-    // function onMessage(message) {
-    //   q.push(message, function (err, result) {
-    //     if (err) { Logger.error(err); return }
-    //   })
-    //   consumer.pause(); //Pause kafka consumer group to not receive any more new messages
-    // }
-
-    consumer.on('message', (message) => {
-      Logger.info(`kafkaConsumer:: Consumed from ${message.topic} consumed: ${JSON.stringify(message)}`)
-
-      // var payload = JSON.parse(message.value)
-
-      q.push(message, function (err, result) {
-        if (err) { Logger.error(err); return }
-      })
-      consumer.pause() // Pause kafka consumer group to not receive any more new messages
-
-      // funcProcessMessage(payload).then(result => {
-      //   if (result) {
-      //     // Logger.info('result: %s', result)
-      //     consumer.commit(
-      //       function (err, result) {
-      //         if (err) {
-      //           Logger.info('kafkaConsumer:: Committing index error: %s', (JSON.stringify(err) || JSON.stringify(result)))
-      //         }
-      //         Logger.info('kafkaConsumer:: Committing index for %s', message.topic)
-      //       })
-      //   }
-      // }).catch(reason => {
-      //   Logger.error(`kafkaConsumer:: funcProcessMessage(${funcProcessMessage.name}) failed with the following reason: ${reason}`)
-      // })
-    })
-
-    consumer.on('error', function (err) {
-      Logger.error('kafkaConsumer:: ERROR: %s', err.toString())
-    })
+    }, null, true, config.pollingTimeZone)
   }).catch(reason => {
     Logger.error(`kafkaConsumer:: Unable to fetch list topics with regex topicRegexFilter(${topicRegexFilter}) with the following reason: ${reason}`)
-  }
-  )
+  })
 }
 
 exports.register = (server, options, next) => {
-  // reLoadConsumersJob.start()
   const kafkaOptions = Config.TOPICS_KAFKA_CONSUMER_OPTIONS
+  const kafkaConfig = Config.TOPICS_KAFKA_CONSUMER_CONFIG
 
-  kafkaConsumer('PREPARE_TX', Commands.prepareExecute, Config.TOPICS_PREPARE_TX_REGEX, kafkaOptions)
-  kafkaConsumer('PREPARE_NOTIFICATIONS', (msg) => {
+  kafkaConsumers(Commands.prepareExecute, Config.TOPICS_PREPARE_TX_REGEX, kafkaOptions, kafkaConfig)
+  kafkaConsumers((msg) => {
     return new Promise((resolve, reject) => {
       Logger.info(`Message process for Notifications ${JSON.stringify(msg)}`)
       return resolve(true)
     })
-  }, Config.TOPICS_PREPARE_NOTIFICATION_REGEX, kafkaOptions)
+  }, Config.TOPICS_PREPARE_NOTIFICATION_REGEX, kafkaOptions, kafkaConfig)
   next()
 }
 
