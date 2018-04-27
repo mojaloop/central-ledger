f9490c7409b535e5833e8586785b8e48a3b10369
Henk Kodde <henk.kodde@modusbox.com>
2018-04-27 20:59:32 +0200

    #210 - transfer settlement batch table updated V1.0.3

--- migrations/20180418101700_create-transfer-settlement-batch.js
+++ migrations/20180418101700_create-transfer-settlement-batch.js
@@ -1,13 +1,13 @@
 'use strict'
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTableIfNotExists('transferBatch', (t) => {
-        t.bigIncrements('transferBatchId').primary().notNullable()
+    return knex.schema.createTableIfNotExists('transferSettlementBatch', (t) => {
+        t.bigIncrements('transferSettlementBatchId').primary().notNullable()
         t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable()
         t.string('state', 50).defaultTo(null).nullable()
     })
 }
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('transferBatch')
+    return knex.schema.dropTableIfExists('transferSettlementBatch')
 }
