63c1530673bf1e7f739cc5460b407bf7c0f29701
Miguel de Barros <miguel@debarros.me>
2018-05-04 11:11:01 +0200

    Several updates for transgers, ilp and transferstatechange
    1. projection for transfers now correctly inserts transfer, ilp and transferStateChange into DB
    2. updated states
    3. moved ilp model to generic model folder

--- src/domain/transfer/models/transfers-read-model.js
+++ src/domain/transfer/models/transfers-read-model.js
@@ -10,15 +10,15 @@ const findExpired = (expiresAt) => {
   return Db.transfer.find({ state: TransferState.PREPARED, 'expirationDate <': expirationDate })
 }
 
-const saveTransfer = (record) => {
+const saveTransfer = async (record) => {
   Logger.debug('save transfer' + record.toString())
-  return Db.transfer.insert(record).catch(err => {
+  return await Db.transfer.returning('transferId').insert(record).catch(err => {
     throw err
   })
 }
 
-const getAll = () => {
-  return Db.transfer.query(builder => {
+const getAll = async () => {
+  return await Db.transfer.query(builder => {
     return builder
       .innerJoin('participant AS ca', 'transfer.payerParticipantId', 'ca.participantId')
       .innerJoin('participant AS da', 'transfer.payeeParticipantId', 'da.participantId')
@@ -41,19 +41,19 @@ const getAll = () => {
   })
 }
 
-const updateTransfer = (transferId, fields) => {
-  return Db.transfer.update({ transferId: transferId }, fields).catch(err => {
+const updateTransfer = async (transferId, fields) => {
+  return await Db.transfer.update({ transferId: transferId }, fields).catch(err => {
     Logger.info(err)
     throw err
   })
 }
 
-const truncateTransfers = () => {
-  return Db.transfer.truncate()
+const truncateTransfers = async () => {
+  return await Db.transfer.truncate()
 }
 
-const getById = (id) => {
-  return Db.transfer.query(builder => {
+const getById = async (id) => {
+  return await Db.transfer.query(builder => {
     return builder
       .where({ transferId: id })
       .innerJoin('participant AS ca', 'transfer.payerParticipantId', 'ca.participantId')
--- src/domain/transfer/projection.js
+++ src/domain/transfer/projection.js
@@ -1,55 +1,80 @@
 'use strict'
 
 const _ = require('lodash')
-const UrlParser = require('../../lib/urlparser')
+// const UrlParser = require('../../lib/urlparser')
 const Util = require('../../lib/util')
+// const Moment = require('moment')
 const ParticipantService = require('../../domain/participant')
 const TransferState = require('./state')
 const TransferRejectionType = require('./rejection-type')
-const TransfersReadModel = require('./models/transfers-read-model')
+const TransfersModel = require('./models/transfers-read-model')
+const ilpModel = require('../../models/ilp')
+const transferStateChangeModel = require('./models/transferStateChanges')
 const ExecuteTransfersModel = require('../../models/executed-transfers')
 const SettledTransfersModel = require('../../models/settled-transfers')
 
 const saveTransferPrepared = async (payload) => {
-  const debitParticipant = await UrlParser.nameFromParticipantUri(payload.debits[0].participant)
-  const creditParticipant = await UrlParser.nameFromParticipantUri(payload.credits[0].participant)
-  const names = [debitParticipant, creditParticipant]
-  const participants = []
-  for (var i = 0, len = names.length; i < len; i++) {
-    const participant = await ParticipantService.getByName(names[i])
+  // const debitParticipant = await UrlParser.nameFromParticipantUri(payload.debits[0].participant)
+  // const creditParticipant = await UrlParser.nameFromParticipantUri(payload.credits[0].participant)
+  // const names = [debitParticipant, creditParticipant]
+
+  const participants = {}
+
+  // for (var i = 0, len = names.length; i < len; i++) {
+  //   const participant = await ParticipantService.getByName(names[i])
+  //   participants.push(participant)
+  // }
+
+  const names = [payload.payeeFsp, payload.payerFsp]
+
+  names.forEach(async (name) => {
+    const participant = await ParticipantService.getByName(name)
     participants.push(participant)
-  }
+  })
+
   const participantIds = await _.reduce(participants, (m, acct) => _.set(m, acct.name, acct.participantId), {})
-  const record = {
-    transferId: payload.id,
-    state: TransferState.PREPARED,
-    ledger: payload.ledger,
-    payeeParticipantId: participantIds[debitParticipant],
-    payeeAmount: payload.debits[0].amount,
-    payeeNote: JSON.stringify(payload.debits[0].memo),
-    payerParticipantId: participantIds[creditParticipant],
-    payerAmount: payload.credits[0].amount,
-    payerNote: JSON.stringify(payload.credits[0].memo),
-    payeeRejected: 0,
-    payeeRejectionMessage: null,
-    executionCondition: payload.execution_condition,
-    cancellationCondition: payload.cancellation_condition,
-    fulfillment: null,
-    rejectionReason: payload.rejection_reason,
-    expirationDate: new Date(payload.expires_at),
-    additionalInfo: payload.additional_info,
-    preparedDate: payload.timeline.prepared_at,
-    executedDate: null,
-    rejectedDate: null
+
+  const transferRecord = {
+    transferId: payload.transferId,
+    payeeParticipantId: participantIds[payload.payeeFsp],
+    payerParticipantId: participantIds[payload.payerFsp],
+    amount: payload.amount.amount,
+    currencyId: payload.amount.currency,
+    expirationDate: new Date(payload.expiration)
   }
 
-  await TransfersReadModel.saveTransfer(record).catch(err => {
+  const ilpRecord = {
+    transferId: payload.transferId,
+    packet: payload.ilpPacket,
+    condition: payload.condition,
+    fulfillment: null
+  }
+
+  const transferStateRecord = {
+    transferId: payload.transferId,
+    transferStateId: TransferState.RECEIVED,
+    reason: 'Received Transfer for Prepare',
+    changedDate: (new Date()).toISOString()
+  }
+
+  // TODO: ExtensionList
+  // TODO: Move inserts into a Transaction
+
+  await ilpModel.saveIlp(ilpRecord).catch(err => {
     throw new Error(err.message)
   })
 
-  record.creditParticipantName = creditParticipant
-  record.debitParticipantName = debitParticipant
-  return record
+  await transferStateChangeModel.saveTransferStateChange(transferStateRecord).catch(err => {
+    throw new Error(err.message)
+  })
+
+  await TransfersModel.saveTransfer(transferRecord).catch(err => {
+    throw new Error(err.message)
+  })
+
+  // transferRecord.creditParticipantName = creditParticipant
+  // transferRecord.debitParticipantName = debitParticipant
+  return { transferRecord, ilpRecord, transferStateRecord }
 }
 
 const saveTransferExecuted = async ({payload, timestamp}) => {
@@ -58,7 +83,7 @@ const saveTransferExecuted = async ({payload, timestamp}) => {
     fulfillment: payload.fulfillment,
     executedDate: new Date(timestamp)
   }
-  return await TransfersReadModel.updateTransfer(payload.id, fields)
+  return await TransfersModel.updateTransfer(payload.id, fields)
 }
 
 const saveTransferRejected = async ({aggregate, payload, timestamp}) => {
@@ -73,7 +98,7 @@ const saveTransferRejected = async ({aggregate, payload, timestamp}) => {
       payeeRejectionMessage: payload.message || ''
     })
   }
-  return await TransfersReadModel.updateTransfer(aggregate.id, fields)
+  return await TransfersModel.updateTransfer(aggregate.id, fields)
 }
 
 const saveExecutedTransfer = async (transfer) => {
