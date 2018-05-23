b5b82c1ab000f543864b4f356b1132aebfdf3fca
Rajiv Mothilal <rajiv.mothilal@modusbox.com>
2018-05-23 15:53:54 +0200

    fixed object for transfer

--- src/handlers/lib/kafka/testProducer.js
+++ src/handlers/lib/kafka/testProducer.js
@@ -8,7 +8,7 @@ const Uuid = require('uuid4')
 const Utility = require('../utility')
 
 const transfer = {
-  transferId: 'b51ec534-ee48-4575-b6a9-ead2955b8999',
+  transferId: 'b51ec534-ee48-4575-b6a9-ead2955b8111',
   payerFsp: 'dfsp1',
   payeeFsp: 'dfsp2',
   amount: {
@@ -16,8 +16,8 @@ const transfer = {
     amount: '433.88'
   },
   ilpPacket: 'AYIBgQAAAAAAAASwNGxldmVsb25lLmRmc3AxLm1lci45T2RTOF81MDdqUUZERmZlakgyOVc4bXFmNEpLMHlGTFGCAUBQU0svMS4wCk5vbmNlOiB1SXlweUYzY3pYSXBFdzVVc05TYWh3CkVuY3J5cHRpb246IG5vbmUKUGF5bWVudC1JZDogMTMyMzZhM2ItOGZhOC00MTYzLTg0NDctNGMzZWQzZGE5OGE3CgpDb250ZW50LUxlbmd0aDogMTM1CkNvbnRlbnQtVHlwZTogYXBwbGljYXRpb24vanNvbgpTZW5kZXItSWRlbnRpZmllcjogOTI4MDYzOTEKCiJ7XCJmZWVcIjowLFwidHJhbnNmZXJDb2RlXCI6XCJpbnZvaWNlXCIsXCJkZWJpdE5hbWVcIjpcImFsaWNlIGNvb3BlclwiLFwiY3JlZGl0TmFtZVwiOlwibWVyIGNoYW50XCIsXCJkZWJpdElkZW50aWZpZXJcIjpcIjkyODA2MzkxXCJ9IgA',
-  condition: 'YlK5TZyhflbXaDRPtR5zhCu8FrbgvrQwwmzuH0iQ0AI',
-  expiration: '2016-05-24T08:38:08.699-04:00',
+  condition: 'ni:///sha-256;47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU?fpt=preimage-sha-256&cost=0',
+  expiration: '2018-11-24T08:38:08.699-04:00',
   extensionList: {
     extension: [
       {
@@ -56,22 +56,22 @@ const messageProtocol = {
   pp: ''
 }
 
-// const topicConf = {
-//   topicName: Utility.transformAccountToTopicName(transfer.payerFsp, 'transfer', 'prepare'),
-//   key: 'producerTest',
-//   partition: 0,
-//   opaqueKey: 0
-// }
-
 const topicConf = {
-  topicName: Utility.transformGeneralTopicName('transfer', 'transfer'),
+  topicName: Utility.transformAccountToTopicName(transfer.payerFsp, 'transfer', 'prepare'),
   key: 'producerTest',
   partition: 0,
   opaqueKey: 0
 }
 
+// const topicConf = {
+//   topicName: Utility.transformGeneralTopicName('transfer', 'transfer'),
+//   key: 'producerTest',
+//   partition: 0,
+//   opaqueKey: 0
+// }
+
 exports.testProducer = async () => {
-  const config = Utility.getKafkaConfig(Utility.ENUMS.PRODUCER, 'TRANSFER', 'TRANSFER')
+  const config = Utility.getKafkaConfig(Utility.ENUMS.PRODUCER, 'TRANSFER', 'PREPARE')
   config.logger = Logger
   await Producer.produceMessage(messageProtocol, topicConf, config)
   return true
