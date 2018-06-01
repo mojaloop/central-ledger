dad408dc0094ad67d4a686621b4a90234999c69d
Nikolay Anastasov <nikolay.anastasov@softwaregroup.com>
2018-06-01 14:46:41 +0300

    improvement of the coverage for ilp

--- testPI2/unit/domain/transfer/ilp/model.test.js
+++ testPI2/unit/domain/transfer/ilp/model.test.js
@@ -53,14 +53,26 @@ Test('Ilp model', async (ilpTest) => {
     insert: sandbox.stub(),
     update: sandbox.stub(),
     findOne: sandbox.stub(),
-    find: sandbox.stub()
+    find: sandbox.stub(),
+    query: sandbox.stub(),
+    destroy: sandbox.stub()
   }
 
   await ilpTest.test('create false ilp', async (assert) => {
-    const falseIlp = {transferId: '1'}
+    const falseIlp = {
+      transferId: '1',
+      packet: '',
+      condition: '',
+      fulfilment: ''
+    }
+
     Db.ilp.insert.withArgs({
-      transferId: falseIlp.transferId
-    }).throws(new Error('message'))
+      transferId: falseIlp.transferId,
+      packet: falseIlp.packet,
+      condition: falseIlp.condition,
+      fulfilment: falseIlp.fulfilment
+    }).throws(new Error('False insert ilp'))
+
     try {
       let r = await Model.create(falseIlp)
       assert.comment(r)
@@ -90,7 +102,7 @@ Test('Ilp model', async (ilpTest) => {
   })
 
   await ilpTest.test('get with empty transferId', async (assert) => {
-    Db.ilp.findOne.withArgs({ transferId: '' }).throws(new Error())
+    Db.ilp.query.throws(new Error('False getByTransferId ilp'))
     try {
       await Model.getByTransferId('')
       assert.fail(' should throws with empty transferId ')
@@ -100,20 +112,42 @@ Test('Ilp model', async (ilpTest) => {
     assert.end()
   })
 
-  // await ilpTest.test('getByTransferId', async (assert) => {
-  //   try {
-  //     Db.ilp.select.withArgs({ transferId: '1' }).returns(ilpTestValues[0])
-  //     var result = await Model.getByTransferId('1')
-  //     assert.equal(result.transferId, ilp.transferId, ' transferIds are equal')
-  //     assert.equal(result.condition, ilp.condition, ' conditions match')
-  //     assert.equal(result.fulfilment, ilp.fulfilment, ' fulfillments match')
-  //     assert.end()
-  //   } catch (err) {
-  //     Logger.error(`create ilp failed with error - ${err}`)
-  //     assert.fail()
-  //     assert.end()
-  //   }
-  // })
+  await ilpTest.test('getByTransferId', async (assert) => {
+    Db.ilp.query.returns(ilpTestValues[0])
+    try {
+      var result = await Model.getByTransferId('1')
+      assert.equal(result.transferId, ilp.transferId, ' transferIds are equal')
+      assert.equal(result.condition, ilp.condition, ' conditions match')
+      assert.equal(result.fulfilment, ilp.fulfilment, ' fulfillments match')
+      assert.end()
+    } catch (err) {
+      Logger.error(`create ilp failed with error - ${err}`)
+      assert.fail()
+      assert.end()
+    }
+  })
+
+  await ilpTest.test('update false ilp', async (assert) => {
+    const falseIlp = {
+      transferId: '',
+      ilpId: '',
+      packet: '',
+      condition: '',
+      fulfilment: ''
+    }
+
+    Db.ilp.update.withArgs({
+      ilpId: falseIlp.ilpId
+    }).throws(new Error('False update ilp'))
+
+    try {
+      await Model.update(falseIlp, falseIlp)
+      assert.fail(' should throws with empty transferId ')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
+    }
+    assert.end()
+  })
 
   await ilpTest.test('update', async (assert) => {
     try {
@@ -122,6 +156,44 @@ Test('Ilp model', async (ilpTest) => {
       ).returns(ilpId)
       let updatedId = await Model.update({ ilpId: ilpId }, { packet: 'new test packet' })
       assert.equal(updatedId, ilpId)
+      assert.end()
+    } catch (err) {
+      Logger.error(`update ilp failed with error - ${err}`)
+      assert.fail(`update ilp failed with error - ${err}`)
+      assert.end()
+    }
+  })
+
+  await ilpTest.test('destroyByTransferId false ilp', async (assert) => {
+    const falseIlp = {
+      transferId: '',
+      ilpId: '',
+      packet: '',
+      condition: '',
+      fulfilment: ''
+    }
+
+    Db.ilp.destroy.withArgs({
+      transferId: falseIlp.transferId
+    }).throws(new Error('False destroyByTransferId ilp'))
+
+    try {
+      await Model.destroyByTransferId({ transferId: falseIlp.transferId })
+      assert.fail(' should throws with empty transferId ')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
+    }
+    assert.end()
+  })
+
+  await ilpTest.test('destroyByTransferId', async (assert) => {
+    try {
+      Db.ilp.destroy.withArgs(
+        { transferId: ilpTestValues[0].transferId }
+      ).returns(1)
+
+      let updatedId = await Model.destroyByTransferId({ transferId: ilpTestValues[0].transferId })
+      assert.equal(1, updatedId)
       sandbox.restore()
       assert.end()
     } catch (err) {
