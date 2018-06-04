be493e37a1d72e43f861248357f82b880dda0b21
Valentin Genev <vgenev@gmail.com>
2018-06-04 12:15:48 +0300

    removed some commented code and added license to the transfer-read-model also

--- src/models/ilp.js
+++ src/models/ilp.js
@@ -26,9 +26,7 @@
 'use strict'
 
 const Db = require('../db')
-// const Moment = require('moment')
 const Util = require('../lib/util')
-// const Time = require('../../../lib/time')
 
 exports.saveIlp = async (transfer) => {
   try {
@@ -43,57 +41,13 @@ exports.saveIlp = async (transfer) => {
   }
 }
 
-// exports.getById = async (id) => {
-//   try {
-//     return await Db.ilp.findOne({ ilpId: id })
-//   } catch (err) {
-//     throw new Error(err.message)
-//   }
-// }
-
-// exports.getByTransferId = async (transferId) => {
-//   try {
-//     return await Db.ilp.query(async (builder) => {
-//       return builder
-//         .where({'ilp.transferId': transferId})
-//         // .where('expirationDate', '>', `${Time.getCurrentUTCTimeInMilliseconds()}`)
-//         .innerJoin('transfer as transfer', 'transfer.transferId', 'ilp.transferId')
-//         .select('ilp.*')
-//         .select('transfer.*')
-//         .first()
-//     })
-//     // return await Db.ilp
-//     //   .findOne({ transferId: transferId })
-//     //   .innerJoin('transfer', 'transfer.transferId', 'ilp.transferId')
-//     //   .where('expirationDate', '>', `${Time.getCurrentUTCTimeInMilliseconds()}`) // or maybe ${Moment.utc().toISOString()}
-//   } catch (err) {
-//     throw new Error(err.message)
-//   }
-// }
-
 exports.getByTransferId = async (transferId) => {
   try {
     return await Db.ilp.findOne({ transferId: transferId })
-      // .innerJoin('transfer', 'transfer.transferId', 'ilp.transferId')
-      // .where('expirationDate', '>', `${Time.getCurrentUTCTimeInMilliseconds()}`) // or maybe ${Moment.utc().toISOString()}
   } catch (err) {
     throw new Error(err.message)
   }
 }
-
-// exports.update = async (ilp, payload) => {
-//   const fields = {
-//     transferId: ilp.transferId,
-//     packet: payload.packet || payload.ilpPacket,
-//     condition: payload.condition,
-//     fulfilment: payload.fulfilment
-//   }
-//   try {
-//     return await Db.ilp.update({ilpId: ilp.ilpId}, Util.filterUndefined(fields))
-//   } catch (err) {
-//     throw new Error(err.message)
-//   }
-// }
 
 exports.update = async (ilp) => {
   const fields = {
