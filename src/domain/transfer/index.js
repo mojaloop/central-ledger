72c7a92c7c35fc3a25dd3f087b4afa459185a400
bothadeon <deon.botha@modusbox.com>
2018-06-06 13:25:37 +0200

    Add jsdocs block tags to methods in src/domain/transfer/index.js

--- src/domain/transfer/index.js
+++ src/domain/transfer/index.js
@@ -1,4 +1,38 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * Lazola Lucas <lazola.lucas@modusbox.com>
+ * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+ * Miguel de Barros <miguel.debarros@modusbox.com>
+
+ --------------
+ ******/
 'use strict'
+
+/**
+ * @module src/domain/transfer
+ */
 
 const P = require('bluebird')
 const TransferQueries = require('./queries')
@@ -11,14 +45,41 @@ const State = require('./state')
 const Events = require('../../lib/events')
 const Errors = require('../../errors')
 
+/**
+ * @function getById
+ *
+ * @description Queries the transfer, participant, transferStateChange and ilp tables with the given transfer Id
+ * Returns a transferResult object from the database
+ * @param {string} id Participant Id
+ * @returns {Object} transferResult object
+ */
 const getById = (id) => {
   return TransferQueries.getById(id)
 }
 
+/**
+ * @function getAll
+ *
+ * @description Queries the transfer, participant, transferStateChange and ilp tables
+ * Returns a transferResult list from the database
+ * @returns {Object|Array} List of transferResults
+ */
 const getAll = () => {
   return TransferQueries.getAll()
 }
 
+/**
+ * @function getFulfillment
+ *
+ * @description Gets the fulfillment by transfer id
+ * Returns a transfer fulfilment
+ * @param {string} id Participant Id
+ * @returns {Promise} Promise object representing a transfer fulfilment
+ * @throws {TransferNotFoundError}
+ * @throws {TransferNotConditionalError}
+ * @throws {AlreadyRolledBackError}
+ * @throws {MissingFulfillmentError}
+ */
 const getFulfillment = (id) => {
   return getById(id)
     .then(transfer => {
@@ -62,6 +123,14 @@ const expire = (id) => {
   return reject({id, rejection_reason: RejectionType.EXPIRED})
 }
 
+/**
+ * @function fulfil
+ *
+ * @description Registers the handler for each participant topic created. Gets Kafka config from default.json
+ *
+ * Calls createHandler to register the handler against the Stream Processing API
+ * @returns {boolean} - Returns a boolean: true if successful, or throws and error if failed
+ */
 const fulfil = (fulfilment) => {
   return Commands.fulfil(fulfilment)
     .then(transfer => {
