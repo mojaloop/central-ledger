832779fda924404def98a33c0627b74e0861541d
ggrg <georgi.georgiev@modusbox.com>
2018-07-03 15:25:54 +0300

    Delete deprecated and update first 3 scripts

--- migrations/110200_currency.js
+++ migrations/110200_currency.js
@@ -4,6 +4,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('currency', (t) => {
     t.string('currencyId', 3).primary().notNullable()
     t.string('name', 128).defaultTo(null).nullable()
+    t.boolean('isActive').defaultTo(true).notNullable()
+    t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
