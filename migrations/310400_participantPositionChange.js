5b3a68ae62b59f20321a32b59081b78a92ec4434
ggrg <georgi.georgiev@modusbox.com>
2018-07-03 22:24:04 +0300

    Migration scripts 28, 30

--- migrations/310400_participantPositionChange.js
+++ migrations/310400_participantPositionChange.js
@@ -3,9 +3,9 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('participantPositionChange', (t) => {
     t.bigIncrements('participantPositionChangeId').primary().notNullable()
-    t.big('participantPositionId').notNullable()
+    t.bigInteger('participantPositionId').notNullable()
     t.foreign('participantPositionId').references('participantPositionId').inTable('participantPosition')
-    t.big('transferStateChangeId').notNullable()
+    t.bigInteger('transferStateChangeId').notNullable()
     t.foreign('transferStateChangeId').references('transferStateChangeId').inTable('transferStateChange')
     t.decimal('value', 18, 2).notNullable()
     t.decimal('reservedValue', 18, 2).notNullable()
--- migrations/400200_settlementWindow.js
+++ migrations/400200_settlementWindow.js
@@ -3,8 +3,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('settlementWindow', (t) => {
     t.bigIncrements('settlementWindowId').primary().notNullable()
-    t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable()
     t.string('state', 50).defaultTo(null).nullable()
+    t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable()
   })
 }
 
