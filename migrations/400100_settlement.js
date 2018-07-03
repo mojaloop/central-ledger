999c22f2820262bbac065c6bab8a78708b985a89
bothadeon <deon.botha@modusbox.com>
2018-07-03 20:30:02 +0200

    Migration scripts 29

--- migrations/400100_settlement.js
+++ migrations/400100_settlement.js
@@ -3,10 +3,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('settlement', (t) => {
     t.bigIncrements('settlementId').primary().notNullable()
-
     t.bigInteger('settlementWindowId').unsigned().notNullable()
     t.foreign('settlementWindowId').references('settlementWindowId').inTable('settlementWindow')
-
     t.string('settlementType', 16).notNullable()
     t.dateTime('settledDate').defaultTo(knex.fn.now()).notNullable()
   })
