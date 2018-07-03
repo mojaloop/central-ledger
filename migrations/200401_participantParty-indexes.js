9f28e5c4b6d379931c72e56d21e068a9fd986aeb
bothadeon <deon.botha@modusbox.com>
2018-07-03 19:36:07 +0200

    Migration scripts 17-21

--- migrations/200401_participantParty-indexes.js
+++ migrations/200401_participantParty-indexes.js
@@ -3,7 +3,6 @@
 exports.up = function (knex, Promise) {
   return knex.schema.table('participantParty', (t) => {
     t.index('participantId')
-    t.index('partyId')
     t.unique(['participantId', 'partyId'])
   })
 }
@@ -11,7 +10,6 @@ exports.up = function (knex, Promise) {
 exports.down = function (knex, Promise) {
   return knex.schema.table('participantParty', (t) => {
     t.dropIndex('participantId')
-    t.dropIndex('partyId')
     t.dropUnique(['participantId', 'partyId'])
   })
 }
--- migrations/200500_participantSettlement.js
+++ migrations/200500_participantSettlement.js
@@ -3,10 +3,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('participantSettlement', (t) => {
     t.bigIncrements('participantSettlementId').primary().notNullable()
-
     t.integer('participantId').unsigned().notNullable()
     t.foreign('participantId').references('participantId').inTable('participant')
-
     t.string('participantNumber', 16).notNullable()
     t.string('routingNumber', 16).notNullable()
   })
