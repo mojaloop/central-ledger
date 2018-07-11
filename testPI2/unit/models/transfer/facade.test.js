8acc49c34c9c3fd9d59a25a7211a8a78006d7bf8
rmothilal <rajivmothilal@gmail.com>
2018-07-12 01:25:54 +0200

    some fixes but still broken tests

--- testPI2/unit/models/transfer/facade.test.js
+++ testPI2/unit/models/transfer/facade.test.js
@@ -1,29 +1,29 @@
 /*****
-License
---------------
-Copyright © 2017 Bill & Melinda Gates Foundation
-The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-Contributors
---------------
-This is the official list of the Mojaloop project contributors for this file.
-Names of the original copyright holders (individuals or organizations)
-should be listed with a '*' in the first column. People who have
-contributed from an organization can be listed under the organization
-that actually holds the copyright for their contributions (see the
-Gates Foundation organization for an example). Those individuals should have
-their names indented and be marked with a '-'. Email address can be added
-optionally within square brackets <email>.
-* Gates Foundation
-- Name Surname <name.surname@gatesfoundation.com>
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
 
-* Georgi Georgiev <georgi.georgiev@modusbox.com>
-* Valentin Genev <valentin.genev@modusbox.com>
-* Rajiv Mothilal <rajiv.mothilal@modusbox.com>
-* Miguel de Barros <miguel.debarros@modusbox.com>
---------------
-******/
+ * Georgi Georgiev <georgi.georgiev@modusbox.com>
+ * Valentin Genev <valentin.genev@modusbox.com>
+ * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
+ * Miguel de Barros <miguel.debarros@modusbox.com>
+ --------------
+ ******/
 
 // TODO
 
@@ -120,100 +120,6 @@ Test('Transfer model', async (transferTest) => {
     t.end()
   })
 
-// saveTransfer
-  await transferTest.test('save transfer test', async (assert) => {
-    try {
-      let saved = {transferId: transferRecord.transferId}
-      Db.transfer.insert.returns(Promise.resolve(saved))
-      let transferCreated = await Model.saveTransfer(payload, null, null)
-      assert.equal(transferCreated, saved, 'transfer is inserted and Id is returned')
-      assert.ok(Db.transfer.insert.calledOnce)
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.fail()
-      assert.end()
-    }
-  })
-
-  await transferTest.test('save transfer test should throw an error', async (assert) => {
-    try {
-      let saved = {transferId: transferRecord.transferId}
-      Db.transfer.insert.throws(new Error())
-      let transferCreated = await Model.saveTransfer(payload, null, null)
-      assert.equal(transferCreated, saved, 'transfer is inserted and Id is returned')
-      assert.ok(Db.transfer.insert.calledOnce)
-      assert.fail('Error not thrown')
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.pass('Error thrown')
-      assert.end()
-    }
-  })
-
-// updateTransfer
-  await transferTest.test('updateTransfer should', async (assert) => {
-    try {
-      let fields = {state: TransferState.EXECUTED, fulfilment: 'oAKAAA'}
-      let updatedTransfer = {transferId: payload.transferId}
-      Db.transfer.update = sandbox.stub().returns(Promise.resolve(updatedTransfer))
-      let u = await Model.updateTransfer(payload.transferId, fields)
-      assert.equal(u, updatedTransfer)
-      assert.ok(Db.transfer.update.calledWith({transferId: payload.transferId}, fields))
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.fail()
-      assert.end()
-    }
-  })
-
-  await transferTest.test('updateTransfer should throw error', async (assert) => {
-    try {
-      let fields = {state: TransferState.EXECUTED, fulfilment: 'oAKAAA'}
-      let updatedTransfer = {transferId: payload.transferId}
-      Db.transfer.update = sandbox.stub().throws(new Error())
-      let u = await Model.updateTransfer(payload.transferId, fields)
-      assert.equal(u, updatedTransfer)
-      assert.ok(Db.transfer.update.calledWith({transferId: payload.transferId}, fields))
-      assert.fail('Error not thrown')
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.pass('Error thrown')
-      assert.end()
-    }
-  })
-
-// truncateTransfer
-  await transferTest.test('truncateTransfers should', async (assert) => {
-    try {
-      Db.transfer.truncate.returns(Promise.resolve())
-      await Model.truncateTransfers()
-      assert.ok(Db.transfer.truncate.calledOnce)
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.fail()
-      assert.end()
-    }
-  })
-
-  await transferTest.test('truncateTransfers should throw an error', async (assert) => {
-    try {
-      Db.transfer.truncate.throws(new Error())
-      await Model.truncateTransfers()
-      assert.ok(Db.transfer.truncate.calledOnce)
-      assert.fail('Error not thrown')
-      assert.end()
-    } catch (err) {
-      Logger.error(`create transfer failed with error - ${err}`)
-      assert.pass('Error thrown')
-      assert.end()
-    }
-  })
-
 // getById
   await transferTest.test('return transfer by id', async (assert) => {
     try {
@@ -256,22 +162,32 @@ Test('Transfer model', async (transferTest) => {
 
       let found = await Model.getById(transferId1)
       assert.equal(found, transfers)
-      assert.ok(builderStub.where.withArgs({'transfer.transferId': transferId1}))
-      assert.ok(payerStub.withArgs('participant AS ca', 'transfer.payerParticipantId', 'ca.participantId').calledOnce)
-      assert.ok(payeeStub.withArgs('participant AS da', 'transfer.payeeParticipantId', 'da.participantId').calledOnce)
+      assert.ok(builderStub.where.withArgs({
+        'transfer.transferId': transferId1,
+        'tprt1.name': 'PAYER_DFSP',
+        'tprt2.name': 'PAYEE_DFSP'
+      }))
+      assert.ok(payerStub.withArgs('transferParticipant AS tp1', 'tp1.transferId', 'transfer.transferId').calledOnce)
+      assert.ok(payeeStub.withArgs('transferParticipantRoleType AS tprt1', 'tprt1.transferParticipantRoleTypeId', 'tp1.transferParticipantRoleTypeId').calledOnce)
       assert.ok(stateChangeStub.withArgs('transferStateChange AS tsc', 'transfer.transferId', 'tsc.transferId').calledOnce)
       assert.ok(ilpStub.withArgs('ilp AS ilp', 'transfer.transferId', 'ilp.transferId').calledOnce)
       assert.ok(selectStub.withArgs(
         'transfer.*',
         'transfer.currencyId AS currency',
-        'ca.name AS payerFsp',
-        'da.name AS payeeFsp',
+        'pc1.participantCurrencyId AS payerParticipantCurrencyId',
+        'tp1.amount AS payerAmount',
+        'da.participantId AS payerParticipantId',
+        'da.name AS payerFsp',
+        'pc2.participantCurrencyId AS payeeParticipantCurrencyId',
+        'tp2.amount AS payeeAmount',
+        'ca.participantId AS payeeParticipantId',
+        'ca.name AS payeeFsp',
         'tsc.transferStateId AS transferState',
-        'tsc.changedDate AS completedTimestamp',
-        'ilp.packet AS ilpPacket',
-        'ilp.condition AS condition',
-        'ilp.fulfilment AS fulfilment',
-        'ilp.ilpId AS ilpId'
+        'tsc.reason AS reason',
+        'tsc.createdDate AS completedTimestamp',
+        'ilpp.value AS ilpPacket',
+        'transfer.ilpCondition AS condition',
+        'tf.ilpFulfilment AS fulfilment'
       ).calledOnce)
       assert.ok(orderStub.withArgs('tsc.transferStateChangeId', 'desc').calledOnce)
       assert.ok(firstStub.withArgs().calledOnce)
@@ -305,51 +221,60 @@ Test('Transfer model', async (transferTest) => {
       const transfers = [{transferId: transferId1}, {transferId: transferId2}]
 
       let builderStub = sandbox.stub()
+      let payerStub = sandbox.stub()
       let payeeStub = sandbox.stub()
       let stateChangeStub = sandbox.stub()
       let stateStub = sandbox.stub()
       let ilpStub = sandbox.stub()
       let selectStub = sandbox.stub()
       let orderStub = sandbox.stub()
-
-      builderStub.innerJoin = sandbox.stub()
+      builderStub.where = sandbox.stub()
 
       Db.transfer.query.callsArgWith(0, builderStub)
       Db.transfer.query.returns(transfers)
 
-      builderStub.innerJoin.returns({
-        innerJoin: payeeStub.returns({
-          leftJoin: stateChangeStub.returns({
-            leftJoin: stateStub.returns({
-              leftJoin: ilpStub.returns({
+      builderStub.where.returns({
+        innerJoin: payerStub.returns({
+          innerJoin: payeeStub.returns({
+            innerJoin: stateStub.returns({
+              innerJoin: ilpStub.returns({
                 select: selectStub.returns({
                   orderBy: orderStub.returns(transfers)
                 })
               })
             })
+
           })
         })
       })
 
       let found = await Model.getAll()
       assert.equal(found, transfers)
-      assert.ok(builderStub.innerJoin.withArgs('participant AS ca', 'transfer.payerParticipantId', 'ca.participantId').calledOnce)
-      assert.ok(payeeStub.withArgs('participant AS da', 'transfer.payeeParticipantId', 'da.participantId').calledOnce)
-      assert.ok(stateChangeStub.withArgs('transferStateChange AS tsc', 'transfer.transferId', 'tsc.transferId').calledOnce)
-      assert.ok(stateStub.withArgs('transferState AS ts', 'ts.transferStateId', 'tsc.transferStateId').calledOnce)
-      assert.ok(ilpStub.withArgs('ilp AS ilp', 'transfer.transferId', 'ilp.transferId').calledOnce)
+      assert.ok(builderStub.where.withArgs({
+        'tprt1.name': 'PAYER_DFSP',
+        'tprt2.name': 'PAYEE_DFSP'
+      }))
+      assert.ok(payerStub.withArgs('transferParticipant AS tp1', 'tp1.transferId', 'transfer.transferId').calledOnce)
+      assert.ok(payeeStub.withArgs('transferParticipantRoleType AS tprt1', 'tprt1.transferParticipantRoleTypeId', 'tp1.transferParticipantRoleTypeId').calledOnce)
+      assert.ok(stateStub.withArgs('participant AS da', 'da.participantId', 'pc1.participantId').calledOnce)
+      assert.ok(ilpStub.withArgs('transferParticipant AS tp2', 'tp2.transferId', 'transfer.transferId').calledOnce)
       assert.ok(selectStub.withArgs(
         'transfer.*',
         'transfer.currencyId AS currency',
-        'ca.name AS payerFsp',
-        'da.name AS payeeFsp',
-        'tsc.transferStateId AS internalTransferState',
-        'tsc.changedDate AS completedTimestamp',
-        'ts.enumeration AS transferState',
-        'ilp.packet AS ilpPacket',
-        'ilp.condition AS condition',
-        'ilp.fulfilment AS fulfilment',
-        'ilp.ilpId AS ilpId'
+        'pc1.participantCurrencyId AS payerParticipantCurrencyId',
+        'tp1.amount AS payerAmount',
+        'da.participantId AS payerParticipantId',
+        'da.name AS payerFsp',
+        'pc2.participantCurrencyId AS payeeParticipantCurrencyId',
+        'tp2.amount AS payeeAmount',
+        'ca.participantId AS payeeParticipantId',
+        'ca.name AS payeeFsp',
+        'tsc.transferStateId AS transferState',
+        'tsc.reason AS reason',
+        'tsc.createdDate AS completedTimestamp',
+        'ilpp.value AS ilpPacket',
+        'transfer.ilpCondition AS condition',
+        'tf.ilpFulfilment AS fulfilment'
       ).calledOnce)
       assert.ok(orderStub.withArgs('tsc.transferStateChangeId', 'desc').calledOnce)
       sandbox.restore()
@@ -397,7 +322,11 @@ Test('Transfer model', async (transferTest) => {
 
       let found = await Model.getAll()
       assert.equal(found, transfers)
-      assert.ok(builderStub.innerJoin.withArgs('participant AS ca', 'transfer.payerParticipantId', 'ca.participantId').calledOnce)
+      assert.ok(builderStub.where.withArgs({
+        'transfer.transferId': transferId1,
+        'tprt1.name': 'PAYER_DFSP',
+        'tprt2.name': 'PAYEE_DFSP'
+      }))
       assert.ok(payeeStub.withArgs('participant AS da', 'transfer.payeeParticipantId', 'da.participantId').calledOnce)
       assert.ok(stateChangeStub.withArgs('transferStateChange AS tsc', 'transfer.transferId', 'tsc.transferId').calledOnce)
       assert.ok(stateStub.withArgs('transferState AS ts', 'tsc.transferStateId', 'tsc.transferStateId').calledOnce)
@@ -414,7 +343,7 @@ Test('Transfer model', async (transferTest) => {
         'ilp.condition AS condition',
         'ilp.fulfilment AS fulfilment',
         'ilp.ilpId AS ilpId'
-  ).calledOnce)
+      ).calledOnce)
       assert.ok(orderStub.withArgs('tsc.=transferStateChangeId', 'desc').calledOnce)
       sandbox.restore()
       assert.fail('Error not thrown')
