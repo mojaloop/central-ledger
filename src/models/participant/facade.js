21ac3d1d7d6837e6fe8ad665bec03413c6bf2083
shashi165 <33355509+shashi165@users.noreply.github.com>
2018-07-18 13:14:51 -0500

    story #351 Add/Get callback details for participants (#91)
    * story #351 changes
    
    * changes requested in PR review
    
    * merged latest code from develop-PI3
    
    * changed suggested in PR review

--- src/models/participant/facade.js
+++ src/models/participant/facade.js
@@ -24,6 +24,10 @@
 
 'use strict'
 
+/**
+ * @module src/models/participant/
+ */
+
 const Db = require('../../db')
 
 const getByNameAndCurrency = async (name, currencyId) => {
@@ -47,6 +51,118 @@ const getByNameAndCurrency = async (name, currencyId) => {
   }
 }
 
+/**
+ * @function GetEndpoint
+ *
+ * @async
+ * @description This retuns the active endpoint value for a give participantId and type of endpoint
+ *
+ *
+ * @param {integer} participantId - the id of the participant in the database. Example 1
+ * @param {string} type - the type of the endpoint. Example 'FSIOP_CALLBACK_URL'
+ *
+ * @returns {array} - Returns participantEndpoint array containing the details of active endpoint for the participant if successful, or throws an error if failed
+ */
+
+const getEndpoint = async (participantId, endpointType) => {
+  try {
+    return Db.participantEndpoint.query(builder => {
+      return builder.innerJoin('endpointType AS et', 'participantEndpoint.endpointTypeId', 'et.endpointTypeId')
+        .where({
+          'participantEndpoint.participantId': participantId,
+          'participantEndpoint.isActive': 1,
+          'et.name': endpointType
+        }).select('participantEndpoint.*',
+          'et.name')
+    })
+  } catch (err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @function GetAllEndpoints
+ *
+ * @async
+ * @description This retuns all the active endpoints for a give participantId
+ *
+ *
+ * @param {integer} participantId - the id of the participant in the database. Example 1
+ *
+ * @returns {array} - Returns an array containing the list of all active endpoints for the participant if successful, or throws an error if failed
+ */
+
+const getAllEndpoints = async (participantId) => {
+  try {
+    return Db.participantEndpoint.query(builder => {
+      return builder.innerJoin('endpointType AS et', 'participantEndpoint.endpointTypeId', 'et.endpointTypeId')
+        .where({
+          'participantEndpoint.participantId': participantId,
+          'participantEndpoint.isActive': 1
+        }).select('participantEndpoint.*',
+          'et.name')
+    })
+  } catch (err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @function AddEndpoint
+ *
+ * @async
+ * @description This adds the endpoint details for a participant into the database
+ *
+ * If there is an existing active endpoint for the give participant and endpointType, That endpoing will be made inactive,
+ * by updating the database entry isActive = 0.
+ * Then new endpoint entry will be inserted into the database, all this will happen inside a database transaction to maintaing the database integrity
+ *
+ * @param {integer} participantId - the participant id. Example: 1
+ * @param {object} payload - the payload containing and endpoint object with 'type' and 'value' of the endpoint.
+ * Example: {
+ *      "endpoint": {
+ *      "type": "FSIOP_CALLBACK_URL",
+ *      "value": "http://localhost:3001/participants/dfsp1/notification12"
+ *    }
+ * }
+ * @returns {integer} - Returns number of database rows affected if successful, or throws an error if failed
+ */
+
+const addEndpoint = async (participantId, endpoint) => {
+  try {
+    const knex = Db.getKnex()
+    return knex.transaction(async trx => {
+      let endpointType = await trx.first('endpointTypeId').from('endpointType').where({ 'name': endpoint.type, 'isActive': 1 })
+      return knex('participantEndpoint').transacting(trx).forUpdate().select('*')
+        .where({
+          'participantId': participantId,
+          'endpointTypeId': endpointType.endpointTypeId,
+          'isActive': 1
+        })
+        .then(existingEndpoint => {
+          if (Array.isArray(existingEndpoint) && existingEndpoint.length > 0) {
+            return knex('participantEndpoint').transacting(trx).update({ isActive: 0 }).where('participantEndpointId', existingEndpoint[0].participantEndpointId)
+          }
+        }).then(() => {
+          let newEndpoint = {
+            participantId: participantId,
+            endpointTypeId: endpointType.endpointTypeId,
+            value: endpoint.value,
+            isActive: 1,
+            createdBy: 'unknown'
+          }
+          return knex('participantEndpoint').transacting(trx).insert(newEndpoint)
+        }).then(trx.commit)
+        .catch(trx.rollback)
+    })
+  } catch (err) {
+    throw new Error(err.message)
+  }
+}
+
 module.exports = {
-  getByNameAndCurrency
+  getByNameAndCurrency,
+  getEndpoint,
+  getAllEndpoints,
+  addEndpoint
 }
--- testPI2/unit/domain/participant/index.test.js
+++ testPI2/unit/domain/participant/index.test.js
@@ -9,7 +9,7 @@
  --------------
  This is the official list of the Mojaloop project contributors for this file.
  Names of the original copyright holders (individuals or organizations)
- should be listed with a '*'in the first column. People who have
+ should be listed with a '*' in the first column. People who have
  contributed from an organization can be listed under the organization
  that actually holds the copyright for their contributions (see the
  Gates Foundation organization for an example). Those individuals should have
@@ -17,8 +17,6 @@
  optionally within square brackets <email>.
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
-
- * Georgi Georgiev <georgi.georgiev@modusbox.com>
  * Valentin Genev <valentin.genev@modusbox.com>
  * Rajiv Mothilal <rajiv.mothilal@modusbox.com>
  * Miguel de Barros <miguel.debarros@modusbox.com>
@@ -33,6 +31,7 @@ const Db = require('../../../../src/db/index')
 const Logger = require('@mojaloop/central-services-shared').Logger
 const ParticipantModel = require('../../../../src/models/participant/participant')
 const ParticipantCurrencyModel = require('../../../../src/models/participant/participantCurrency')
+const ParticipantFacade = require('../../../../src/models/participant/facade')
 
 const Service = require('../../../../src/domain/participant/index')
 
@@ -40,14 +39,14 @@ Test('Participant service', async (participantTest) => {
   let sandbox
   const participantFixtures = [
     {
-      participantId: 1,
+      participantId: 0,
       name: 'fsp1',
       currency: 'USD',
       isActive: 1,
       createdDate: new Date()
     },
     {
-      participantId: 2,
+      participantId: 1,
       name: 'fsp2',
       currency: 'EUR',
       isActive: 1,
@@ -56,7 +55,7 @@ Test('Participant service', async (participantTest) => {
   ]
   const participantResult = [
     {
-      participantId: 1,
+      participantId: 0,
       name: 'fsp1',
       currency: 'USD',
       isActive: 1,
@@ -64,7 +63,7 @@ Test('Participant service', async (participantTest) => {
       currencyList: 'USD'
     },
     {
-      participantId: 2,
+      participantId: 1,
       name: 'fsp2',
       currency: 'EUR',
       isActive: 1,
@@ -74,16 +73,39 @@ Test('Participant service', async (participantTest) => {
   ]
   const participantCurrencyResult = [
     {
-      participantCurrancyId: 1,
-      participantId: 1,
+      participantCurrancyId: 0,
+      participantId: 0,
       currencyId: 'USD',
       isActive: 1
     },
     {
-      participantCurrancyId: 2,
-      participantId: 2,
+      participantCurrancyId: 1,
+      participantId: 1,
       currencyId: 'EUR',
       isActive: 1
+    }
+  ]
+
+  const endpoints = [
+    {
+      participantEndpointId: 1,
+      participantId: 0,
+      endpointTypeId: 1,
+      value: 'http://localhost:3001/participants/dfsp1/notification1',
+      isActive: 1,
+      createdDate: '2018-07-11',
+      createdBy: 'unknown',
+      name: 'FSIOP_CALLBACK_URL'
+    },
+    {
+      participantEndpointId: 2,
+      participantId: 0,
+      endpointTypeId: 2,
+      value: 'http://localhost:3001/participants/dfsp1/notification2',
+      isActive: 1,
+      createdDate: '2018-07-11',
+      createdBy: 'unknown',
+      name: 'ALARM_NOTIFICATION_URL'
     }
   ]
 
@@ -97,11 +119,16 @@ Test('Participant service', async (participantTest) => {
     sandbox.stub(ParticipantModel, 'getById')
     sandbox.stub(ParticipantModel, 'update')
     sandbox.stub(ParticipantModel, 'destroyByName')
+    sandbox.stub(ParticipantModel, 'destroyPariticpantEndpointByParticipantId')
 
     sandbox.stub(ParticipantCurrencyModel, 'create')
     sandbox.stub(ParticipantCurrencyModel, 'getByParticipantId')
     sandbox.stub(ParticipantCurrencyModel, 'getById')
     sandbox.stub(ParticipantCurrencyModel, 'destroyByParticipantId')
+
+    sandbox.stub(ParticipantFacade, 'getEndpoint')
+    sandbox.stub(ParticipantFacade, 'getAllEndpoints')
+    sandbox.stub(ParticipantFacade, 'addEndpoint')
 
     Db.participant = {
       insert: sandbox.stub(),
@@ -120,12 +147,12 @@ Test('Participant service', async (participantTest) => {
 
     participantFixtures.forEach((participant, index) => {
       participantMap.set(index + 1, participantResult[index])
-      Db.participant.insert.withArgs({participant}).returns(index)
-      ParticipantModel.create.withArgs({name: participant.name}).returns((index + 1))
+      Db.participant.insert.withArgs({ participant }).returns(index)
+      ParticipantModel.create.withArgs({ name: participant.name }).returns((index + 1))
       ParticipantModel.getByName.withArgs(participant.name).returns(participantResult[index])
       ParticipantModel.getById.withArgs(index).returns(participantResult[index])
       ParticipantModel.update.withArgs(participant, 1).returns((index + 1))
-      ParticipantCurrencyModel.create.withArgs({participantId: index, currencyId: participant.currency}).returns((index + 1))
+      ParticipantCurrencyModel.create.withArgs({ participantId: index, currencyId: participant.currency }).returns((index + 1))
       ParticipantCurrencyModel.getById.withArgs(index).returns({
         participantCurrancyId: participant.participantId,
         participantId: participant.participantId,
@@ -135,7 +162,7 @@ Test('Participant service', async (participantTest) => {
       ParticipantCurrencyModel.getByParticipantId.withArgs(participant.participantId).returns(participant.currency)
       ParticipantModel.destroyByName.withArgs(participant.name).returns(Promise.resolve(true))
       ParticipantCurrencyModel.destroyByParticipantId.withArgs(participant.participantId).returns(Promise.resolve(true))
-      Db.participant.destroy.withArgs({name: participant.name}).returns(Promise.resolve(true))
+      Db.participant.destroy.withArgs({ name: participant.name }).returns(Promise.resolve(true))
     })
     ParticipantModel.getAll.returns(Promise.resolve(participantResult))
     t.end()
@@ -147,7 +174,7 @@ Test('Participant service', async (participantTest) => {
   })
 
   await participantTest.test('create false participant', async (assert) => {
-    const falseParticipant = {name: 'fsp3'}
+    const falseParticipant = { name: 'fsp3' }
     ParticipantModel.create.withArgs(falseParticipant).throws(new Error())
     try {
       await Service.create(falseParticipant)
@@ -161,7 +188,7 @@ Test('Participant service', async (participantTest) => {
   await participantTest.test('create participant', async (assert) => {
     try {
       for (let [index, participant] of participantMap) {
-        var result = await Service.create({name: participant.name})
+        var result = await Service.create({ name: participant.name })
         assert.comment(`Testing with participant \n ${JSON.stringify(participant, null, 2)}`)
         assert.ok(Sinon.match(result, index + 1), `returns ${result}`)
       }
@@ -193,7 +220,7 @@ Test('Participant service', async (participantTest) => {
         assert.equal(result.name, participant.name, 'names are equal')
         assert.equal(result.currency, participant.currency, 'currencies match')
         assert.equal(result.isActive, participant.isActive, 'isActive flag match')
-        assert.equal(result.currencyList, participantMap.get(participant.participantId).currencyList, 'currencyList match')
+        assert.equal(result.currencyList, participantMap.get(participant.participantId + 1).currencyList, 'currencyList match')
         assert.ok(Sinon.match(result.createdDate, participant.createdDate), 'created date matches')
       })
       assert.end()
@@ -246,7 +273,7 @@ Test('Participant service', async (participantTest) => {
   await participantTest.test('update', async (assert) => {
     try {
       participantFixtures.forEach(async (participant, index) => {
-        let updated = await Service.update(participant.name, {isActive: 1})
+        let updated = await Service.update(participant.name, { isActive: 1 })
         assert.equal(JSON.stringify(updated), JSON.stringify(participantMap.get(index + 1)))
       })
       //  sandbox.restore()
@@ -262,7 +289,7 @@ Test('Participant service', async (participantTest) => {
   await participantTest.test('update should throw an error', async (assert) => {
     ParticipantModel.getByName.withArgs(participantFixtures[0].name).throws(new Error())
     try {
-      await Service.update(participantFixtures[0].name, {isActive: 1})
+      await Service.update(participantFixtures[0].name, { isActive: 1 })
       assert.fail('Error not thrown')
       assert.end()
     } catch (err) {
@@ -275,7 +302,7 @@ Test('Participant service', async (participantTest) => {
   await participantTest.test('createParticipantCurrency should create the currency', async (assert) => {
     try {
       participantFixtures.forEach(async (participant, index) => {
-        var result = await Service.createParticipantCurrency({participantId: participant.participantId, currencyId: participant.currency})
+        var result = await Service.createParticipantCurrency({ participantId: participant.participantId, currencyId: participant.currency })
         assert.comment(`Testing with participant \n ${JSON.stringify(participant, null, 2)}`)
         assert.ok(Sinon.match(result, index + 1), `returns ${result}`)
       })
@@ -288,10 +315,10 @@ Test('Participant service', async (participantTest) => {
   })
 
   await participantTest.test('createParticipantCurrency with false participant should fail', async (assert) => {
-    const falseParticipant = {name: 'fsp3', participantId: 3, currency: 'FAKE'}
-    ParticipantCurrencyModel.create.withArgs({participantId: falseParticipant.participantId, currencyId: falseParticipant.currency}).throws(new Error())
+    const falseParticipant = { name: 'fsp3', participantId: 3, currency: 'FAKE' }
+    ParticipantCurrencyModel.create.withArgs({ participantId: falseParticipant.participantId, currencyId: falseParticipant.currency }).throws(new Error())
     try {
-      await Service.createParticipantCurrency({participantId: falseParticipant.participantId, currencyId: falseParticipant.currency})
+      await Service.createParticipantCurrency({ participantId: falseParticipant.participantId, currencyId: falseParticipant.currency })
       assert.fail('should throw')
     } catch (err) {
       assert.assert(err instanceof Error, `throws ${err} `)
@@ -314,7 +341,7 @@ Test('Participant service', async (participantTest) => {
   })
 
   await participantTest.test('getParticipantCurrencyById with false participant should fail', async (assert) => {
-    const falseParticipant = {name: 'fsp3', participantId: 3, currency: 'FAKE'}
+    const falseParticipant = { name: 'fsp3', participantId: 3, currency: 'FAKE' }
     ParticipantCurrencyModel.getById.withArgs(falseParticipant.currency).throws(new Error())
     try {
       await Service.getParticipantCurrencyById(falseParticipant.currency)
@@ -342,13 +369,145 @@ Test('Participant service', async (participantTest) => {
 
   await participantTest.test('destroyByName should throw an error', async (assert) => {
     try {
-      const falseParticipant = {name: 'fsp3', participantId: 3, currency: 'FAKE'}
+      const falseParticipant = { name: 'fsp3', participantId: 3, currency: 'FAKE' }
       ParticipantModel.getByName.withArgs(falseParticipant.name).returns(falseParticipant)
       ParticipantModel.destroyByName.withArgs(falseParticipant.name).throws(new Error())
       await Service.destroyByName(falseParticipant.name)
       assert.fail('should throw')
     } catch (err) {
       assert.assert(err instanceof Error, `throws ${err}`)
+    }
+    assert.end()
+  })
+
+  await participantTest.test('getEndpoint', async (assert) => {
+    try {
+      const endpoint = {
+        type: 'FSIOP_CALLBACK_URL',
+        value: 'http://localhost:3001/participants/dfsp1/notification1'
+      }
+      ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+      ParticipantFacade.getEndpoint.withArgs(participantFixtures[0].participantId, endpoint.type).returns([endpoints[0]])
+      const result = await Service.getEndpoint(participantFixtures[0].name, endpoint.type)
+      assert.deepEqual(result[0], endpoints[0], 'Results matched')
+      assert.end()
+    } catch (err) {
+      Logger.error(`get endpoint failed with error - ${err}`)
+      assert.fail()
+      assert.end()
+    }
+  })
+
+  await participantTest.test('getEndpoint should fail if no endpoints found', async (assert) => {
+    const endpoint = {
+      type: 'FSIOP_CALLBACK_URL',
+      value: 'http://localhost:3001/participants/dfsp1/notification1'
+    }
+    ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+    ParticipantFacade.getEndpoint.withArgs(participantFixtures[0].participantId, endpoint.type).returns(null)
+
+    try {
+      await Service.getEndpoint(participantFixtures[0], endpoint.type)
+      assert.fail(' should throw')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
+    }
+    assert.end()
+  })
+
+  await participantTest.test('getAllEndpoints', async (assert) => {
+    try {
+      ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+      ParticipantFacade.getAllEndpoints.withArgs(participantFixtures[0].participantId).returns(endpoints)
+      const result = await Service.getAllEndpoints(participantFixtures[0].name)
+      assert.deepEqual(result, endpoints, 'Results matched')
+      assert.end()
+    } catch (err) {
+      Logger.error(`getAllEndpoints failed with error - ${err}`)
+      assert.fail()
+      assert.end()
+    }
+  })
+
+  await participantTest.test('getAllEndpoints should throw error', async (assert) => {
+    ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+    ParticipantFacade.getAllEndpoints.withArgs(participantFixtures[0].participantId).throws(new Error())
+
+    try {
+      await Service.getAllEndpoints(participantFixtures[0].name)
+      assert.fail(' should throw')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
+    }
+    assert.end()
+  })
+
+  await participantTest.test('addEndpoint', async (assert) => {
+    try {
+      const payload = {
+        type: 'FSIOP_CALLBACK_URL',
+        value: 'http://localhost:3001/participants/dfsp1/notification1'
+      }
+      ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+      ParticipantFacade.addEndpoint.withArgs(participantFixtures[0].participantId, payload).returns(1)
+      const result = await Service.addEndpoint(participantFixtures[0].name, payload)
+      assert.deepEqual(result, 1, 'Results matched')
+      assert.end()
+    } catch (err) {
+      Logger.error(`addEndpoint failed with error - ${err}`)
+      assert.fail()
+      assert.end()
+    }
+  })
+
+  await participantTest.test('addEndpoint should fail if cant add endpoint', async (assert) => {
+    const payload = {
+      type: 'FSIOP_CALLBACK_URL',
+      value: 'http://localhost:3001/participants/dfsp1/notification1'
+    }
+    ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+    ParticipantFacade.addEndpoint.withArgs(participantFixtures[0].participantId, payload).throws(new Error())
+
+    try {
+      await Service.addEndpoint(participantFixtures[0].name, payload)
+      assert.fail(' should throw')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
+    }
+    assert.end()
+  })
+
+  await participantTest.test('destroyPariticpantEndpointByName', async (assert) => {
+    try {
+      ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+      ParticipantModel.destroyPariticpantEndpointByParticipantId.withArgs(participantFixtures[0].participantId).returns(true)
+      const result = await Service.destroyPariticpantEndpointByName(participantFixtures[0].name)
+      assert.equal(result, true, 'Results matched')
+      assert.end()
+    } catch (err) {
+      Logger.error(`destroyPariticpantEndpointByName failed with error - ${err}`)
+      assert.fail()
+      assert.end()
+    }
+  })
+
+  await participantTest.test('destroyPariticpantEndpointByName should fail', async (assert) => {
+    ParticipantModel.getByName.withArgs(participantFixtures[0].name).returns(participantFixtures[0])
+
+    ParticipantModel.destroyPariticpantEndpointByParticipantId.withArgs(participantFixtures[0].participantId).throws(new Error())
+
+    try {
+      await Service.destroyPariticpantEndpointByName(participantFixtures[0].name)
+      assert.fail(' should throw')
+    } catch (err) {
+      assert.assert(err instanceof Error, ` throws ${err} `)
     }
     assert.end()
   })
