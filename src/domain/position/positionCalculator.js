c545d193ecf6fde4526d1dab35178724122ea244
ggrg <georgi.georgiev@modusbox.com>
2018-07-29 01:43:34 +0300

    Adding more unit tests to improve coverage

--- src/domain/position/positionCalculator.js
+++ src/domain/position/positionCalculator.js
@@ -2,8 +2,8 @@
 
 exports.sum = function (position1, position2) {
   return {
-    payments: position1.payments.plus(position2.payments),
-    receipts: position1.receipts.plus(position2.receipts),
-    net: position1.net.plus(position2.net)
+    payments: position1.payments + position2.payments,
+    receipts: position1.receipts + position2.receipts,
+    net: position1.net + position2.net
   }
 }
