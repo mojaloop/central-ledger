e5483a9845963c29e56c2678bff8c5dbefec7468
Miguel de Barros <miguel@debarros.me>
2018-08-03 17:41:53 +0100

    Fixed script formatting.

--- testPI2/util/scripts/restart.sh
+++ testPI2/util/scripts/restart.sh
@@ -10,10 +10,10 @@ CWD="${0%/*}"
 echo "Loading env vars..."
 source $CWD/env.sh
 
-sh $CWD/restartDb.sh ;
+sh $CWD/restartDb.sh;
 
 sh $CWD/restartKafka-johnnypark.sh;
 
-sh $CWD/restartMockServer.sh
+sh $CWD/restartMockServer.sh;
 
 echo
