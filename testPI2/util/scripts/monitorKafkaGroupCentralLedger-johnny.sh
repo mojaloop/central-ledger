a34ce905e96c47253f41eba887fd7f5f1fa76fe5
Miguel de Barros <miguel@debarros.me>
2018-09-14 19:55:03 +0100

    Fixed issue with helper scripts
    Fixed helper scripts:
    1. Updated expectations for Mock-Server to correctly work for all requests, and never expire
    2. Updated monitor script for Johnny Park Kafka Container which has been updated with a new version of Kafka
    3. Updated start scripts to correctly print the config end-points for each DFSP

--- testPI2/util/scripts/monitorKafkaGroupCentralLedger-johnny.sh
+++ testPI2/util/scripts/monitorKafkaGroupCentralLedger-johnny.sh
@@ -10,4 +10,4 @@ fi
 echo "Loading env vars..."
 source $CWD/env.sh
 
-docker exec -t $KAFKA_ID sh -c "while true; do ./opt/kafka_2.12-1.1.0/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group central-ledger-kafka --describe; sleep 5; done"
+docker exec -t $KAFKA_ID sh -c "while true; do ./opt/kafka_2.12-1.1.1/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group central-ledger-kafka --describe; sleep 5; done"
