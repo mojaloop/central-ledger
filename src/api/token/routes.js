d98fc1ac4a7e0a84121f6e554fb3460440e76571
rmothilal <rajivmothilal@gmail.com>
2018-10-08 14:54:10 +0200

    fixes for cleaning up central-ledger

--- src/api/token/routes.js
+++ src/api/token/routes.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Handler = require('./handler')
-const ParticipantAuthStrategy = require('../auth/participant')
 const tags = ['api', 'token']
 
 module.exports = [
@@ -11,7 +10,6 @@ module.exports = [
     handler: Handler.create,
     options: {
       tags,
-      auth: ParticipantAuthStrategy.scheme,
       description: 'Get a token that can be used to authenticate future requests',
       id: 'auth_token'
     }
