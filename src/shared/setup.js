e6d25106a5a72ca2bb45a00a4bb10437cb02450b
deonbotha <deon.botha@modusbox.com>
2018-10-16 17:20:44 +0200

    Feature/#452 add get on/transfers/{id} (#176)
    * Added backend code for story #452 - get transfers by Id
    
    * Change default.json back to default
    
    * bug fix
    
    * Added unit test(Pos and neg) for register transfer handler
    
    * Added more unit tests to increase code coverage for Transfer handler
    
    * Add Unit tests and increase code coverage

--- src/shared/setup.js
+++ src/shared/setup.js
@@ -132,6 +132,9 @@ const createHandlers = async (handlers) => {
         case 'timeout':
           await RegisterHandlers.timeouts.registerTimeoutHandler()
           break
+        case 'get':
+          await RegisterHandlers.transfers.registerGetHandler()
+          break
         default:
           var error = `Handler Setup - ${JSON.stringify(handler)} is not a valid handler to register!`
           Logger.error(error)
