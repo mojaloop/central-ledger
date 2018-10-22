2203c544b333522c011919749e312d64e7f4f66b
bothadeon <deon.botha@modusbox.com>
2018-10-22 15:06:39 +0200

    Add unit test addNewCurrencyAndPosition for facade, unhappy path

--- test/unit/models/participant/facade.test.js
+++ test/unit/models/participant/facade.test.js
@@ -977,7 +977,7 @@ Test('Participant facade', async (facadeTest) => {
       assert.end()
     }
   })
-  await facadeTest.test('addNewCurrencyAndPosition ..............', async (assert) => {
+  await facadeTest.test('addNewCurrencyAndPosition.', async (assert) => {
     try {
       let participantPosition = {
         participantCurrencyId: 1,
@@ -1023,6 +1023,55 @@ Test('Participant facade', async (facadeTest) => {
       console.log(err)
       Logger.error(`addNewCurrencyAndPosition failed with error - ${err}`)
       assert.fail('Error thrown')
+      assert.end()
+    }
+  })
+  await facadeTest.test('addNewCurrencyAndPosition should throw an error.', async (assert) => {
+    try {
+      let participantPosition = {
+        participantCurrencyId: 1,
+        value: 0,
+        reservedValue: 0,
+        participantPositionId: 1
+      }
+
+      let participantCurrency = {
+        participantCurrencyId: 1,
+        participantId: 1,
+        currencyId: 1,
+        ledgerAccountTypeId: 1,
+        createdBy: 'unknown'
+      }
+
+      let participant = {
+        participantId: 1,
+        currencyId: 1,
+        ledgerAccountTypeId: 1
+      }
+
+      sandbox.stub(Db, 'getKnex')
+      const knexStub = sandbox.stub()
+      const trxStub = sandbox.stub()
+      trxStub.commit = sandbox.stub()
+      knexStub.transaction = sandbox.stub().callsArgWith(0, trxStub)
+      Db.getKnex.returns(knexStub)
+      // let transactingStub = sandbox.stub()
+      /* knexStub.returns({
+        transacting: transactingStub.returns({
+          insert: sandbox.stub().returns([1])
+        })
+      }) */
+      knexStub.throws(new Error())
+
+      const result = await Model.addNewCurrencyAndPosition(participant.participantId, participant.currencyId, participant.ledgerAccountTypeId)
+      assert.pass('completed successfully')
+      assert.ok(knexStub.withArgs('participantCurrency').calledOnce, 'knex called with participantCurrency once')
+      assert.ok(knexStub.withArgs('participantPosition').calledOnce, 'knex called with participantPosition once')
+      assert.deepEqual(result, { participantCurrency, participantPosition })
+
+      assert.end()
+    } catch (err) {
+      assert.pass('Error thrown')
       assert.end()
     }
   })
