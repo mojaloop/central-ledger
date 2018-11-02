774ea0488ddf2294f5e30b52f0c0489e36527c55
ggrg <georgi.georgiev@modusbox.com>
2018-11-02 12:47:31 +0200

    Bug fixed

--- src/admin/participants/routes.js
+++ src/admin/participants/routes.js
@@ -1,37 +1,50 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * Georgi Georgiev <georgi.georgiev@modusbox.com>
+ --------------
+ ******/
+
 'use strict'
 
 const Handler = require('./handler')
 const Joi = require('joi')
+const currencyList = require('../../../seeds/currency.js').currencyList
 
 const tags = ['api', 'participants']
 const nameValidator = Joi.string().alphanum().min(2).max(30).required().description('Name of the participant')
+const currencyValidator = Joi.string().valid(currencyList).description('Currency code')
 // const passwordValidator = Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required().description('Password for the participant')
-const currencyValidator = Joi.string().valid([
-  'ALL', 'AFN', 'ARS', 'AWG', 'AUD', 'AZN',
-  'BSD', 'BBD', 'BYN', 'BZD', 'BMD', 'BOB', 'BAM', 'BWP', 'BGN', 'BRL', 'BND',
-  'KHR', 'CAD', 'KYD', 'CLP', 'CNY', 'COP', 'CRC', 'HRK', 'CUP', 'CZK',
-  'DKK', 'DOP',
-  'XCD', 'EGP', 'SVC', 'EUR',
-  'FKP', 'FJD',
-  'GHS', 'GIP', 'GTQ', 'GGP', 'GYD',
-  'HNL', 'HKD', 'HUF',
-  'ISK', 'INR', 'IDR', 'IRR', 'IMP', 'ILS',
-  'JMD', 'JPY', 'JEP',
-  'KZT', 'KPW', 'KRW', 'KGS',
-  'LAK', 'LBP', 'LRD',
-  'MKD', 'MYR', 'MUR', 'MXN', 'MNT', 'MZN',
-  'NAD', 'NPR', 'ANG', 'NZD', 'NIO', 'NGN', 'KPW', 'NOK',
-  'OMR',
-  'PKR', 'PAB', 'PYG', 'PEN', 'PHP', 'PLN',
-  'QAR',
-  'RON', 'RUB',
-  'SHP', 'SAR', 'RSD', 'SCR', 'SGD', 'SBD', 'SOS', 'ZAR', 'KRW', 'LKR', 'SEK', 'CHF', 'SRD', 'SYP',
-  'TWD', 'THB', 'TTD', 'TRY', 'TVD',
-  'UAH', 'GBP', 'USD', 'UYU', 'UZS',
-  'VEF', 'VND',
-  'YER',
-  'ZWD'
-]).description('Currency code of the participant')
+
+/* The following code is used to compare the currency lists: fspiop-rest-v1.0-OpenAPI.yaml vs. seeds/currency.js */
+// const currenciesSpec = ['AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY', 'COP', 'CRC', 'CUC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRO', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SOS', 'SPL', 'SRD', 'STD', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VEF', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWD']
+// currenciesSpec.forEach(value => {
+//   if (currencyList.indexOf(value) === -1) {
+//     console.log(`seeds is missing ${value}`)
+//   }
+// })
+// currencyList.forEach(value => {
+//   if (currenciesSpec.indexOf(value) === -1) {
+//     console.error(`spec is missing ${value}`)
+//   }
+// })
 
 module.exports = [
   {
@@ -219,7 +232,7 @@ module.exports = [
       },
       validate: {
         payload: {
-          currency: Joi.string().required().description('Account currency'), // currencyValidator,
+          currency: currencyValidator,
           type: Joi.string().required().description('Account type')  // Needs a validator here
         },
         params: {
