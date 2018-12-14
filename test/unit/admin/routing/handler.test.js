3866e7baed2ce276bee844e6c44095fa60e268f8
donovan <donovanchangfoot@gmail.com>
2018-12-14 14:31:24 +0200

    feat: add hardcoded nextHop endpoint
    * nextHop endpoint returns cnp as nexthop

---
+++ test/unit/admin/routing/handler.test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const Test = require('tapes')(require('tape'))
+const Handler = require('../../../../src/admin/routing/handler')
+
+Test('routing', routeTest => {
+  routeTest.test('getNextHop returns finalDestination and destination', async function (assert) {
+    const params = {
+      'fspiop-destination': 'moja.tz.red.tz.pink'
+    }
+
+    const response = await Handler.getNextHop({ headers: params })
+    assert.equals(response, {
+      finalDestination: 'moja.tz.red.tz.pink',
+      destination: 'moja.superremit'
+    })
+    assert.end()
+  })
+
+  routeTest.end()
+})
