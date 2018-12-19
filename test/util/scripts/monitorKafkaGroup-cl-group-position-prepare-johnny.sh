8991c67fdf30e5eb8f6926f3314554a736675baa
Miguel de Barros <miguel@debarros.me>
2018-12-19 13:44:18 +0200

    updated stream lib to 4.3.0

---
+++ test/util/scripts/monitorKafkaGroup-cl-group-position-prepare-johnny.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CWD="${0%/*}"
+
+if [[ "$CWD" =~ ^(.*)\.sh$ ]];
+then
+    CWD="."
+fi
+
+echo "Loading env vars..."
+source $CWD/env.sh
+
+docker exec -t $KAFKA_ID sh -c "while true; do ./opt/kafka_2.12-1.1.1/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group cl-group-position-prepare --describe; sleep 5; done"
