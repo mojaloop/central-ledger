4f3cd492ba47ef8c5431abfebe972e127725b0b1
Unknown <shashikant.hirugade@softwaregroup.com>
2019-01-16 21:38:30 -0600

    removed the commented code

--- test/unit/api/metrics/handler.test.js
+++ test/unit/api/metrics/handler.test.js
@@ -72,56 +72,5 @@ Test('metrics handler', (handlerTest) => {
     healthTest.end()
   })
 
-  // handlerTest.test('metrics should', function (metadataTest) {
-  //   metadataTest.test('return 200 httpStatus', async function (t) {
-  //     let reply = {
-  //       response: () => {
-  //         return {
-  //           code: statusCode => {
-  //             t.equal(statusCode, 200)
-  //             t.end()
-  //           }
-  //         }
-  //       }
-  //     }
-  //     await Handler.metadata(createRequest(), reply)
-  //   })
-
-  //   metadataTest.test('return urls from request.server and append hostname', t => {
-  //     let hostName = 'some-host-name'
-  //     Config.HOSTNAME = hostName
-  //     let request = createRequest([
-  //       { settings: { id: 'first_route', tags: apiTags }, path: '/first' }
-  //     ])
-
-  //     let reply = {
-  //       response: (response) => {
-  //         t.equal(response.urls['first_route'], `${hostName}/first`)
-  //         return { code: statusCode => { t.end() } }
-  //       }
-  //     }
-  //     Handler.metadata(request, reply)
-  //   })
-
-  //   metadataTest.test('format url parameters with colons', t => {
-  //     let request = createRequest([
-  //       { settings: { id: 'path', tags: apiTags }, path: '/somepath/{id}' },
-  //       { settings: { id: 'manyargs', tags: apiTags }, path: '/somepath/{id}/{path*}/{test2}/' }
-  //     ])
-
-  //     let reply = {
-  //       response: (response) => {
-  //         t.equal(response.urls['path'], '/somepath/:id')
-  //         t.equal(response.urls['manyargs'], '/somepath/:id/:path*/:test2/')
-  //         return { code: statusCode => { t.end() } }
-  //       }
-  //     }
-
-  //     Handler.metadata(request, reply)
-  //   })
-
-  //   metadataTest.end()
-  // })
-
   handlerTest.end()
 })
