f63e041859ed617a50677dd6f04c55b356a124e4
ggrg <georgi.georgiev@modusbox.com>
2019-01-23 17:52:59 +0200

    Added Lgger logging capability

--- src/admin/participants/handler.js
+++ src/admin/participants/handler.js
@@ -30,6 +30,7 @@ const UrlParser = require('../../lib/urlParser')
 const Config = require('../../lib/config')
 const Enum = require('../../lib/enum')
 const Sidecar = require('../../lib/sidecar')
+const Logger = require('@mojaloop/central-services-shared').Logger
 const Boom = require('boom')
 
 const entityItem = ({ name, createdDate, isActive, currencyList }, ledgerAccountIds) => {
@@ -161,6 +162,11 @@ const update = async function (request) {
   Sidecar.logRequest(request)
   try {
     const updatedEntity = await Participant.update(request.params.name, request.payload)
+    if (request.payload.isActive !== undefined) {
+      const isActiveText = request.payload.isActive ? 'activated' : 'disabled'
+      const changeLog = JSON.stringify(Object.assign({}, request.params, { isActive: request.payload.isActive }))
+      Logger.info(`Participant has been ${isActiveText} :: ${changeLog}`)
+    }
     const ledgerAccountTypes = await request.server.methods.enums('ledgerAccountType')
     const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
     return entityItem(updatedEntity, ledgerAccountIds)
@@ -312,6 +318,11 @@ const updateAccount = async function (request, h) {
       ledgerAccountType: await request.server.methods.enums('ledgerAccountType')
     }
     await Participant.updateAccount(request.payload, request.params, enums)
+    if (request.payload.isActive !== undefined) {
+      const isActiveText = request.payload.isActive ? 'activated' : 'disabled'
+      const changeLog = JSON.stringify(Object.assign({}, request.params, { isActive: request.payload.isActive }))
+      Logger.info(`Participant account has been ${isActiveText} :: ${changeLog}`)
+    }
     return h.response().code(200)
   } catch (err) {
     throw Boom.badRequest(err.message)
---
+++ test/util/scripts/monitorKafkaGroupCEP.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CWD="${0%/*}"
+
+if [[ "$CWD" =~ ^(.*)\.sh$ ]];
+then
+    CWD="."
+fi
+
+echo "Loading env vars..."
+source $CWD/env.sh
+
+docker exec -t $KAFKA_ID sh -c "while true; do ./opt/kafka_2.11-0.10.1.0/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group kafka-cep --describe; sleep 5; done"
