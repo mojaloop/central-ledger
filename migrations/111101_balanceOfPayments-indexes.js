108e5a63c85d7700383cc20d4c4222fb85dae32d
ggrg <georgi.georgiev@modusbox.com>
2019-05-16 17:57:24 +0300

    Adding more licensing info

--- migrations/111101_balanceOfPayments-indexes.js
+++ migrations/111101_balanceOfPayments-indexes.js
@@ -29,7 +29,7 @@
  --------------
  ******/
 
- // Notes: these changes are required for the quoting-service and are not used by central-ledger
+// Notes: these changes are required for the quoting-service and are not used by central-ledger
 'use strict'
 
 exports.up = function (knex, Promise) {
