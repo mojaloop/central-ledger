8155ac2f2e31ed370e922ad1800f3341e6482614
Juan Correa <gibaros@users.noreply.github.com>
2019-07-10 11:40:49 -0700

    Per Sonar recommendations modified code to address 6 bugs and 2 vulnerabilities (#303)
    * Per Sonar modify code to address 6 bugs and 2 vulnerabilities
    
    Thanks @gibaros. Merging this for v7.1.1 release \:D/

--- src/shared/setup.js
+++ src/shared/setup.js
@@ -247,8 +247,6 @@ const initialize = async function ({ service, port, modules = [], runMigrations 
   let server
   switch (service) {
     case 'api':
-      server = await createServer(port, modules)
-      break
     case 'admin':
       server = await createServer(port, modules)
       break
