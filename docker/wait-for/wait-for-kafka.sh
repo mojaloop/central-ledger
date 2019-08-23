3045c96777ffb9ce63e4a133f3ae3a9875f3e395
Miguel de Barros <miguel@debarros.me>
2019-08-23 16:35:30 +0100

    - added a timeout of 240s to the wait-for-kafka.sh script - removed duplicate var from the wait-for.env

--- docker/wait-for/wait-for-kafka.sh
+++ docker/wait-for/wait-for-kafka.sh
@@ -4,4 +4,4 @@ echo "** STARTUP - Checking for Broker connection..."
 
 source /opt/wait-for/wait-for.env
 
-sh /opt/wait-for/wait-for.sh $WAIT_FOR_DB_KAFKA_BROKER -- echo "** STARTUP - Kafka connection successful!"
+sh /opt/wait-for/wait-for.sh $WAIT_FOR_DB_KAFKA_BROKER -t 240 -- echo "** STARTUP - Kafka connection successful!"
