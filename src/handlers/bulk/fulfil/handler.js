mojaloop/central-ledger

commit 69b697354a125532ee7830ce7814fb0735e7e5da

https://github.com/mojaloop/project/issues/939 (#359)
* current WIP

* updates WIP fixing tests

* Fixing tests, mocking kafka consumer seems to be an issue

* Fixes for tests

* changed from istanbul to nyc, fixed broken test

* fixes found during local testing

* changes from standard

* Dependency updates

* fix for integration tests failing

* updated to latest central-services-shared

* no message

* removed extra space

* updated central-services-shared version

* removed bluebird and added native promises to all tests

src/handlers/bulk/fulfil/handler.js
@@ -22,7 +22,9 @@
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
 
- * Georgi Georgiev <georgi.georgiev@modusbox.com>
+ * ModusBox
+ - Georgi Georgiev <georgi.georgiev@modusbox.com>
+ - Rajiv Mothilal <rajiv.mothilal@modusbox.com>
  --------------
  ******/
 'use strict'
@@ -30,12 +32,10 @@
 const AwaitifyStream = require('awaitify-stream')
 const Logger = require('@mojaloop/central-services-shared').Logger
 const BulkTransferService = require('../../../domain/bulkTransfer')
-const Util = require('../../lib/utility')
-const Kafka = require('../../lib/kafka')
+const Util = require('@mojaloop/central-services-shared').Util
+const Kafka = require('@mojaloop/central-services-shared').Util.Kafka
 const Validator = require('../shared/validator')
-const Enum = require('../../../lib/enum')
-const TransferEventType = Enum.transferEventType
-const TransferEventAction = Enum.transferEventAction
+const Enum = require('@mojaloop/central-services-shared').Enum
 const Metrics = require('@mojaloop/central-services-metrics')
 const Config = require('../../../lib/config')
 const BulkTransferModels = require('@mojaloop/central-object-store').Models.BulkTransfer
@@ -44,35 +44,6 @@ const encodePayload = require('@mojaloop/central-services-stream/src/kafka/proto
 const location = { module: 'BulkFulfilHandler', method: '', path: '' } // var object used as pointer
 
 const consumerCommit = true
-// const fromSwitch = true
-
-// TODO: move to a factory function
-const fulfilHandlerMessageProtocol = {
-  value: {
-    id: null,
-    from: null,
-    to: null,
-    type: 'application/json',
-    content: {
-      uriParams: { id: null },
-      headers: null,
-      payload: null
-    },
-    metadata: {
-      event: {
-        id: null,
-        type: 'fulfil',
-        action: 'commit', // TODO: switch to 'bulk-fulfil' flow
-        createdAt: null,
-        state: {
-          status: 'success',
-          code: 0
-        }
-      }
-    }
-  }
-}
-
 /**
  * @function BulkFulfilHandler
  *
@@ -123,8 +94,8 @@ const bulkFulfil = async (error, messages) => {
       histTimerEnd({ success: false, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
       return true
     }
-    const actionLetter = action === TransferEventAction.BULK_COMMIT ? Enum.actionLetter.bulkCommit : Enum.actionLetter.unknown
-    let params = { message, kafkaTopic, consumer }
+    const actionLetter = action === Enum.Events.Event.Action.BULK_COMMIT ? Enum.Events.ActionLetter.bulkCommit : Enum.Events.ActionLetter.unknown
+    let params = { message, kafkaTopic, consumer, decodedPayload: payload }
 
     Logger.info(Util.breadcrumb(location, { path: 'dupCheck' }))
     const isFulfilment = true
@@ -161,12 +132,10 @@ const bulkFulfil = async (error, messages) => {
         // enable async/await operations for the stream
         const streamReader = AwaitifyStream.createReader(indvidualTransfersFulfilStream)
         let doc
-        const transferIds = []
         while ((doc = await streamReader.readAsync()) !== null) {
           const individualTransferFulfil = doc.payload
           const transferId = individualTransferFulfil.transferId
           delete individualTransferFulfil.transferId
-          transferIds.push(transferId)
           const bulkTransferAssociationRecord = {
             transferId,
             bulkTransferId: payload.bulkTransferId,
@@ -180,21 +149,15 @@ const bulkFulfil = async (error, messages) => {
           } else {
             individualTransferFulfil.transferState = Enum.TransferStateEnum.COMMITTED
           }
-
-          const dataUri = encodePayload(JSON.stringify(individualTransferFulfil), headers['content-type'])
-          const msg = Object.assign({}, fulfilHandlerMessageProtocol)
-          msg.value.id = messageId
-          msg.value.from = headers['fspiop-source']
-          msg.value.to = headers['fspiop-destination']
-          msg.value.content.headers = headers
-          msg.value.content.payload = dataUri
-          msg.value.content.uriParams.id = transferId
-          msg.value.metadata.event.id = message.value.metadata.event.id
-          msg.value.metadata.event.createdAt = new Date()
-
+          const dataUri = encodePayload(JSON.stringify(individualTransferFulfil), headers[Enum.Http.Headers.GENERAL.CONTENT_TYPE.value])
+          const metadata = Util.StreamingProtocol.createMetadataWithCorrelatedEventState(message.value.metadata.event.id, Enum.Events.Event.Type.FULFIL, Enum.Events.Event.Action.COMMIT, Enum.Events.EventStatus.SUCCESS.status, Enum.Events.EventStatus.SUCCESS.code, Enum.Events.EventStatus.SUCCESS.description) // TODO: switch action to 'bulk-fulfil' flow
+          const msg = {
+            value: Util.StreamingProtocol.createMessage(messageId, headers[Enum.Http.Headers.FSPIOP.DESTINATION], headers[Enum.Http.Headers.FSPIOP.SOURCE], metadata, headers, dataUri, { id: transferId })
+          }
           params = { message: msg, kafkaTopic, consumer }
-          const producer = { functionality: TransferEventType.FULFIL, action: TransferEventAction.BULK_COMMIT }
-          await Util.proceed(params, { consumerCommit, histTimerEnd, producer })
+          const producer = { functionality: Enum.Events.Event.Type.FULFIL, action: Enum.Events.Event.Action.BULK_COMMIT }
+          await Kafka.proceed(Config.KAFKA_CONFIG, params, { consumerCommit, histTimerEnd, producer })
+          histTimerEnd({ success: true, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
         }
       } catch (err) { // TODO: handle individual transfers streaming error
         Logger.info(Util.breadcrumb(location, `callbackErrorInternal2--${actionLetter}6`))
@@ -234,8 +197,8 @@ const registerBulkFulfilHandler = async () => {
   try {
     const bulkFulfilHandler = {
       command: bulkFulfil,
-      topicName: Util.transformGeneralTopicName(TransferEventType.BULK, TransferEventAction.FULFIL),
-      config: Util.getKafkaConfig(Util.ENUMS.CONSUMER, TransferEventType.BULK.toUpperCase(), TransferEventAction.FULFIL.toUpperCase())
+      topicName: Kafka.transformGeneralTopicName(Config.KAFKA_CONFIG.TOPIC_TEMPLATES.GENERAL_TOPIC_TEMPLATE.TEMPLATE, Enum.Events.Event.Type.BULK, Enum.Events.Event.Action.FULFIL),
+      config: Kafka.getKafkaConfig(Config.KAFKA_CONFIG, Enum.Kafka.Config.CONSUMER, Enum.Events.Event.Type.BULK.toUpperCase(), Enum.Events.Event.Action.FULFIL.toUpperCase())
     }
     bulkFulfilHandler.config.rdkafkaConf['client.id'] = bulkFulfilHandler.topicName
     await Kafka.Consumer.createHandler(bulkFulfilHandler.topicName, bulkFulfilHandler.config, bulkFulfilHandler.command)

src/handlers/bulk/prepare/handler.js
@@ -22,8 +22,10 @@
  * Gates Foundation
  - Name Surname <name.surname@gatesfoundation.com>
 
- * Georgi Georgiev <georgi.georgiev@modusbox.com>
- * Valentin Genev <valentin.genev@modusbox.com>
+ * ModusBox
+ - Georgi Georgiev <georgi.georgiev@modusbox.com>
+ - Valentin Genev <valentin.genev@modusbox.com>
+ - Rajiv Mothilal <rajiv.mothilal@modusbox.com>
  --------------
  ******/
 'use strict'
@@ -31,48 +33,17 @@
 const AwaitifyStream = require('awaitify-stream')
 const Logger = require('@mojaloop/central-services-shared').Logger
 const BulkTransferService = require('../../../domain/bulkTransfer')
-const Util = require('../../lib/utility')
-const Kafka = require('../../lib/kafka')
+const Util = require('@mojaloop/central-services-shared').Util
+const Kafka = require('@mojaloop/central-services-shared').Util.Kafka
 const Validator = require('../shared/validator')
-const Enum = require('../../../lib/enum')
-const TransferEventType = Enum.transferEventType
-const TransferEventAction = Enum.transferEventAction
+const Enum = require('@mojaloop/central-services-shared').Enum
 const Metrics = require('@mojaloop/central-services-metrics')
 const Config = require('../../../lib/config')
 const BulkTransferModels = require('@mojaloop/central-object-store').Models.BulkTransfer
 const encodePayload = require('@mojaloop/central-services-stream/src/kafka/protocol').encodePayload
 
 const location = { module: 'BulkPrepareHandler', method: '', path: '' } // var object used as pointer
-
 const consumerCommit = true
-// const fromSwitch = true
-
-// TODO: move to a factory function
-const prepareHandlerMessageProtocol = {
-  value: {
-    id: null,
-    from: null,
-    to: null,
-    type: 'application/json',
-    content: {
-      headers: null,
-      payload: null
-    },
-    metadata: {
-      event: {
-        id: null,
-        responseTo: null,
-        type: 'transfer',
-        action: 'bulk-prepare',
-        createdAt: null,
-        state: {
-          status: 'success',
-          code: 0
-        }
-      }
-    }
-  }
-}
 
 const getBulkMessage = async (bulkTransferId) => {
   const BulkTransferModel = BulkTransferModels.getBulkTransferModel()
@@ -130,8 +101,8 @@ const bulkPrepare = async (error, messages) => {
       histTimerEnd({ success: false, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
       return true
     }
-    const actionLetter = action === TransferEventAction.BULK_PREPARE ? Enum.actionLetter.bulkPrepare : Enum.actionLetter.unknown
-    let params = { message, kafkaTopic, consumer }
+    const actionLetter = action === Enum.Events.Event.Action.BULK_PREPARE ? Enum.Events.ActionLetter.bulkPrepare : Enum.Events.ActionLetter.unknown
+    let params = { message, kafkaTopic, consumer, decodedPayload: payload }
 
     Logger.info(Util.breadcrumb(location, { path: 'dupCheck' }))
     const { isDuplicateId, isResend } = await BulkTransferService.checkDuplicate(bulkTransferId, payload.hash)
@@ -152,7 +123,7 @@ const bulkPrepare = async (error, messages) => {
       try {
         Logger.info(Util.breadcrumb(location, 'saveBulkTransfer'))
         const participants = { payerParticipantId, payeeParticipantId }
-        /* const state = */ await BulkTransferService.bulkPrepare(payload, participants)
+        await BulkTransferService.bulkPrepare(payload, participants)
       } catch (err) { // TODO: handle insert error
         Logger.info(Util.breadcrumb(location, `callbackErrorInternal1--${actionLetter}5`))
         Logger.info(Util.breadcrumb(location, 'notImplemented'))
@@ -180,20 +151,15 @@ const bulkPrepare = async (error, messages) => {
             bulkProcessingStateId: Enum.BulkProcessingState.RECEIVED
           }
           await BulkTransferService.bulkTransferAssociationCreate(bulkTransferAssociationRecord)
-
-          const dataUri = encodePayload(JSON.stringify(individualTransfer), headers['content-type'])
-          const msg = Object.assign({}, prepareHandlerMessageProtocol)
-          msg.value.id = messageId
-          msg.value.from = payload.payerFsp
-          msg.value.to = payload.payeeFsp
-          msg.value.content.headers = headers
-          msg.value.content.payload = dataUri
-          msg.value.metadata.event.id = message.value.metadata.event.id
-          msg.value.metadata.event.createdAt = new Date()
-
+          const dataUri = encodePayload(JSON.stringify(individualTransfer), headers[Enum.Http.Headers.GENERAL.CONTENT_TYPE.value])
+          const metadata = Util.StreamingProtocol.createMetadataWithCorrelatedEventState(message.value.metadata.event.id, Enum.Events.Event.Type.TRANSFER, Enum.Events.Event.Action.BULK_PREPARE, Enum.Events.EventStatus.SUCCESS.status, Enum.Events.EventStatus.SUCCESS.code, Enum.Events.EventStatus.SUCCESS.description)
+          const msg = {
+            value: Util.StreamingProtocol.createMessage(messageId, headers[Enum.Http.Headers.FSPIOP.DESTINATION], headers[Enum.Http.Headers.FSPIOP.SOURCE], metadata, headers, dataUri)
+          }
           params = { message: msg, kafkaTopic, consumer }
-          const producer = { functionality: TransferEventType.PREPARE, action: TransferEventAction.BULK_PREPARE }
-          await Util.proceed(params, { consumerCommit, histTimerEnd, producer })
+          const producer = { functionality: Enum.Events.Event.Type.PREPARE, action: Enum.Events.Event.Action.BULK_PREPARE }
+          await Kafka.proceed(Config.KAFKA_CONFIG, params, { consumerCommit, producer })
+          histTimerEnd({ success: true, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
         }
       } catch (err) { // TODO: handle individual transfers streaming error
         Logger.info(Util.breadcrumb(location, `callbackErrorInternal2--${actionLetter}6`))
@@ -233,8 +199,8 @@ const registerBulkPrepareHandler = async () => {
   try {
     const bulkPrepareHandler = {
       command: bulkPrepare,
-      topicName: Util.transformGeneralTopicName(TransferEventType.BULK, TransferEventAction.PREPARE),
-      config: Util.getKafkaConfig(Util.ENUMS.CONSUMER, TransferEventType.BULK.toUpperCase(), TransferEventAction.PREPARE.toUpperCase())
+      topicName: Kafka.transformGeneralTopicName(Config.KAFKA_CONFIG.TOPIC_TEMPLATES.GENERAL_TOPIC_TEMPLATE.TEMPLATE, Enum.Events.Event.Type.BULK, Enum.Events.Event.Action.PREPARE),
+      config: Kafka.getKafkaConfig(Config.KAFKA_CONFIG, Enum.Kafka.Config.CONSUMER, Enum.Events.Event.Type.BULK.toUpperCase(), Enum.Events.Event.Action.PREPARE.toUpperCase())
     }
     bulkPrepareHandler.config.rdkafkaConf['client.id'] = bulkPrepareHandler.topicName
     await Kafka.Consumer.createHandler(bulkPrepareHandler.topicName, bulkPrepareHandler.config, bulkPrepareHandler.command)

src/models/bulkTransfer/facade.js
@@ -29,8 +29,8 @@
 
 const Logger = require('@mojaloop/central-services-shared').Logger
 const Db = require('../../lib/db')
-const Enum = require('../../lib/enum')
-const Time = require('../../lib/time')
+const Enum = require('@mojaloop/central-services-shared').Enum
+const Time = require('@mojaloop/central-services-shared').Util.Time
 // const BulkTransferAssociation = require('./BulkTransferAssociation')
 
 const saveBulkTransferReceived = async (payload, participants, stateReason = null, isValid = true) => {
@@ -42,7 +42,7 @@ const saveBulkTransferReceived = async (payload, participants, stateReason = nul
       payeeParticipantId: participants.payeeParticipantId,
       expirationDate: Time.getUTCString(new Date(payload.expiration))
     }
-    const state = (isValid ? Enum.BulkTransferState.RECEIVED : Enum.BulkTransferState.INVALID)
+    const state = (isValid ? Enum.Transfers.BulkTransferState.RECEIVED : Enum.Transfers.BulkTransferState.INVALID)
     const bulkTransferStateChangeRecord = {
       bulkTransferId: payload.bulkTransferId,
       bulkTransferStateId: state,
@@ -87,7 +87,7 @@ const saveBulkTransferProcessing = async (payload, bulkTransferFulfilmentId, sta
     // TODO: Remove count or decide on the strategy how to handle intdividual transfer results. Count is not sufficient criteria
     // because even if it's matched the transferId's may not match those on record.
     // const count = await BulkTransferAssociation.count(payload.bulkTransferId, Enum.BulkProcessingState.ACCEPTED)
-    const state = (isValid /* && payload.count === count */ ? Enum.BulkTransferState.PROCESSING : Enum.BulkTransferState.INVALID)
+    const state = (isValid /* && payload.count === count */ ? Enum.Transfers.BulkTransferState.PROCESSING : Enum.Transfers.BulkTransferState.INVALID)
     const bulkTransferStateChangeRecord = {
       bulkTransferId: payload.bulkTransferId,
       bulkTransferStateId: state,

src/api/participants/handler.js
@@ -27,7 +27,7 @@
 const ParticipantService = require('../../domain/participant')
 const UrlParser = require('../../lib/urlParser')
 const Config = require('../../lib/config')
-const Enum = require('../../lib/enum')
+const Util = require('@mojaloop/central-services-shared').Util
 const Sidecar = require('../../lib/sidecar')
 const Logger = require('@mojaloop/central-services-shared').Logger
 const ErrorHandler = require('@mojaloop/central-services-error-handling')
@@ -92,7 +92,7 @@ const create = async function (request, h) {
       const participantId = await ParticipantService.create(request.payload)
       participant = await ParticipantService.getById(participantId)
     }
-    const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
+    const ledgerAccountIds = Util.transpose(ledgerAccountTypes)
     const participantCurrencyId1 = await ParticipantService.createParticipantCurrency(participant.participantId, request.payload.currency, ledgerAccountTypes.POSITION, false)
     const participantCurrencyId2 = await ParticipantService.createParticipantCurrency(participant.participantId, request.payload.currency, ledgerAccountTypes.SETTLEMENT, false)
     participant.currencyList = [await ParticipantService.getParticipantCurrencyById(participantCurrencyId1), await ParticipantService.getParticipantCurrencyById(participantCurrencyId2)]
@@ -140,7 +140,7 @@ const createHubAccount = async function (request, h) {
     }
     // end here : move to domain
     const ledgerAccountTypes = await request.server.methods.enums('ledgerAccountType')
-    const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
+    const ledgerAccountIds = Util.transpose(ledgerAccountTypes)
     return h.response(entityItem(participant, ledgerAccountIds)).code(201)
   } catch (err) {
     throw ErrorHandler.Factory.reformatFSPIOPError(err)
@@ -150,7 +150,7 @@ const createHubAccount = async function (request, h) {
 const getAll = async function (request) {
   const results = await ParticipantService.getAll()
   const ledgerAccountTypes = await request.server.methods.enums('ledgerAccountType')
-  const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
+  const ledgerAccountIds = Util.transpose(ledgerAccountTypes)
   return results.map(record => entityItem(record, ledgerAccountIds))
 }
 
@@ -158,7 +158,7 @@ const getByName = async function (request) {
   const entity = await ParticipantService.getByName(request.params.name)
   handleMissingRecord(entity)
   const ledgerAccountTypes = await request.server.methods.enums('ledgerAccountType')
-  const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
+  const ledgerAccountIds = Util.transpose(ledgerAccountTypes)
   return entityItem(entity, ledgerAccountIds)
 }
 
@@ -172,7 +172,7 @@ const update = async function (request) {
       Logger.info(`Participant has been ${isActiveText} :: ${changeLog}`)
     }
     const ledgerAccountTypes = await request.server.methods.enums('ledgerAccountType')
-    const ledgerAccountIds = Enum.transpose(ledgerAccountTypes)
+    const ledgerAccountIds = Util.transpose(ledgerAccountTypes)
     return entityItem(updatedEntity, ledgerAccountIds)
   } catch (err) {
     throw ErrorHandler.Factory.reformatFSPIOPError(err)

commit 6f8e3e0a34bf22c57a3b4c6457466af7ec322879

Bugfix: Issues introduced with PR #359

src/handlers/bulk/fulfil/handler.js
@@ -139,15 +139,15 @@ const bulkFulfil = async (error, messages) => {
           const bulkTransferAssociationRecord = {
             transferId,
             bulkTransferId: payload.bulkTransferId,
-            bulkProcessingStateId: Enum.BulkProcessingState.PROCESSING
+            bulkProcessingStateId: Enum.Transfers.BulkProcessingState.PROCESSING
           }
           await BulkTransferService.bulkTransferAssociationUpdate(transferId, bulkTransferId, bulkTransferAssociationRecord)
-          if (state === Enum.BulkTransferState.INVALID ||
+          if (state === Enum.Transfers.BulkTransferState.INVALID ||
             individualTransferFulfil.errorInformation ||
             !individualTransferFulfil.fulfilment) {
-            individualTransferFulfil.transferState = Enum.TransferStateEnum.ABORTED
+            individualTransferFulfil.transferState = Enum.Transfers.TransferState.ABORTED
           } else {
-            individualTransferFulfil.transferState = Enum.TransferStateEnum.COMMITTED
+            individualTransferFulfil.transferState = Enum.Transfers.TransferState.COMMITTED
           }
           const dataUri = encodePayload(JSON.stringify(individualTransferFulfil), headers[Enum.Http.Headers.GENERAL.CONTENT_TYPE.value])
           const metadata = Util.StreamingProtocol.createMetadataWithCorrelatedEventState(message.value.metadata.event.id, Enum.Events.Event.Type.FULFIL, Enum.Events.Event.Action.COMMIT, Enum.Events.EventStatus.SUCCESS.status, Enum.Events.EventStatus.SUCCESS.code, Enum.Events.EventStatus.SUCCESS.description) // TODO: switch action to 'bulk-fulfil' flow

src/handlers/bulk/prepare/handler.js
@@ -148,7 +148,7 @@ const bulkPrepare = async (error, messages) => {
           const bulkTransferAssociationRecord = {
             transferId: individualTransfer.transferId,
             bulkTransferId: payload.bulkTransferId,
-            bulkProcessingStateId: Enum.BulkProcessingState.RECEIVED
+            bulkProcessingStateId: Enum.Transfers.BulkProcessingState.RECEIVED
           }
           await BulkTransferService.bulkTransferAssociationCreate(bulkTransferAssociationRecord)
           const dataUri = encodePayload(JSON.stringify(individualTransfer), headers[Enum.Http.Headers.GENERAL.CONTENT_TYPE.value])

src/handlers/bulk/processing/handler.js
@@ -222,7 +222,7 @@ const bulkProcessing = async (error, messages) => {
         const participants = await BulkTransferService.getParticipantsById(bulkTransferInfo.bulkTransferId)
         const payerBulkResponse = Object.assign({}, { messageId: message.value.id, headers: Util.clone(headers) }, getBulkTransferByIdResult.payerBulkTransfer)
         const payeeBulkResponse = Object.assign({}, { messageId: message.value.id, headers: Util.clone(headers) }, getBulkTransferByIdResult.payeeBulkTransfer)
-        payeeBulkResponse.headers[Enum.Http.Headers.FSPIOP.SOURCE] = Enum.headers.FSPIOP.SWITCH
+        payeeBulkResponse.headers[Enum.Http.Headers.FSPIOP.SOURCE] = Enum.Http.Headers.FSPIOP.SWITCH.value
         payeeBulkResponse.headers[Enum.Http.Headers.FSPIOP.DESTINATION] = participants.payeeFsp
         delete payeeBulkResponse.headers[Enum.Http.Headers.FSPIOP.SIGNATURE]
         const BulkTransferResultModel = BulkTransferModels.getBulkTransferResultModel()
@@ -247,9 +247,9 @@ const bulkProcessing = async (error, messages) => {
         })
         const payeeMetadata = Util.StreamingProtocol.createMetadataWithCorrelatedEvent(params.message.value.metadata.event.id, payeeParams.message.value.metadata.type, payeeParams.message.value.metadata.action, Enum.Events.EventStatus.SUCCESS)
         payeeParams.message.value = Util.StreamingProtocol.createMessage(params.message.value.id, participants.payeeFsp, Enum.Http.Headers.FSPIOP.SWITCH.value, payeeMetadata, payeeBulkResponse.headers, payeePayload)
-        await Kafka.proceed(payerParams, { consumerCommit, producer })
+        await Kafka.proceed(Config.KAFKA_CONFIG, payerParams, { consumerCommit, producer })
         histTimerEnd({ success: true, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
-        await Kafka.proceed(payeeParams, { consumerCommit, producer })
+        await Kafka.proceed(Config.KAFKA_CONFIG, payeeParams, { consumerCommit, producer })
         histTimerEnd({ success: true, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
         return true
       } else {
@@ -258,7 +258,7 @@ const bulkProcessing = async (error, messages) => {
         Logger.info(Util.breadcrumb(location, `invalidEventTypeOrAction--${actionLetter}3`))
         const fspiopError = ErrorHandler.Factory.createInternalServerFSPIOPError(`Invalid event action:(${action}) and/or type:(${eventType})`).toApiErrorObject(Config.ERROR_HANDLING)
         const producer = { functionality: Enum.Events.Event.Type.NOTIFICATION, action: Enum.Events.Event.Action.BULK_PROCESSING }
-        await Kafka.proceed(params, { consumerCommit, fspiopError, producer, fromSwitch })
+        await Kafka.proceed(Config.KAFKA_CONFIG, params, { consumerCommit, fspiopError, producer, fromSwitch })
         histTimerEnd({ success: true, fspId: Config.INSTRUMENTATION_METRICS_LABELS.fspId })
         return true
       }

src/models/bulkTransfer/facade.js
@@ -86,7 +86,7 @@ const saveBulkTransferProcessing = async (payload, bulkTransferFulfilmentId, sta
     }
     // TODO: Remove count or decide on the strategy how to handle intdividual transfer results. Count is not sufficient criteria
     // because even if it's matched the transferId's may not match those on record.
-    // const count = await BulkTransferAssociation.count(payload.bulkTransferId, Enum.BulkProcessingState.ACCEPTED)
+    // const count = await BulkTransferAssociation.count(payload.bulkTransferId, Enum.Transfers.BulkProcessingState.ACCEPTED)
     const state = (isValid /* && payload.count === count */ ? Enum.Transfers.BulkTransferState.PROCESSING : Enum.Transfers.BulkTransferState.INVALID)
     const bulkTransferStateChangeRecord = {
       bulkTransferId: payload.bulkTransferId,