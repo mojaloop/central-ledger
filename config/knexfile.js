8c568bece45d6a3f3ba4724ef151e271de307b58
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-10-08 15:25:32 +0200

    updating to use new database library

--- config/knexfile.js
+++ config/knexfile.js
@@ -8,9 +8,8 @@ const Config = require('../src/lib/config')
 module.exports = {
   client: 'mysql',
   version: '5.5',
-  connection: Config.DATABASE_URI,
-  min: Config.DB_CONNECTION_POOL_MIN,
-  max: Config.DB_CONNECTION_POOL_MAX,
+  connection: Config.DATABASE.connection,
+  pool: Config.DATABASE.pool,
   migrations: {
     directory: migrationsDirectory,
     tableName: 'migration',
