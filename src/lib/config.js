d66a809d7103ee833a034f0512141ce289024311
Rajiv Mothilal <rajivmothilal@gmail.com>
2019-10-09 16:46:21 +0200

    updated database dependency to use config object instead of connection string

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,10 +3,6 @@ const RC = require('rc')('CLEDG', require('../../config/default.json'))
 module.exports = {
   HOSTNAME: RC.HOSTNAME.replace(/\/$/, ''),
   PORT: RC.PORT,
-  // ADMIN_PORT: RC.ADMIN_PORT,
-  // DATABASE_URI: RC.DATABASE_URI,
-  // DB_CONNECTION_POOL_MIN: RC.DB_CONNECTION.POOL_MIN,
-  // DB_CONNECTION_POOL_MAX: RC.DB_CONNECTION.POOL_MAX,
   MONGODB_URI: RC.MONGODB.URI,
   MONGODB_DISABLED: RC.MONGODB.DISABLED,
   AMOUNT: RC.AMOUNT,
@@ -49,27 +45,21 @@ module.exports = {
     pool: {
       // minimum size
       min: RC.DATABASE.POOL_MIN_SIZE ? RC.DATABASE.POOL_MIN_SIZE : 2,
-
       // maximum size
       max: RC.DATABASE.POOL_MAX_SIZE ? RC.DATABASE.POOL_MAX_SIZE : 10,
       // acquire promises are rejected after this many milliseconds
       // if a resource cannot be acquired
       acquireTimeoutMillis: RC.DATABASE.ACQUIRE_TIMEOUT_MILLIS ? RC.DATABASE.ACQUIRE_TIMEOUT_MILLIS : 30000,
-
       // create operations are cancelled after this many milliseconds
       // if a resource cannot be acquired
       createTimeoutMillis: RC.DATABASE.CREATE_TIMEOUT_MILLIS ? RC.DATABASE.CREATE_TIMEOUT_MILLIS : 3000,
-
       // destroy operations are awaited for at most this many milliseconds
       // new resources will be created after this timeout
       destroyTimeoutMillis: RC.DATABASE.DESTROY_TIMEOUT_MILLIS ? RC.DATABASE.DESTROY_TIMEOUT_MILLIS : 5000,
-
       // free resouces are destroyed after this many milliseconds
       idleTimeoutMillis: RC.DATABASE.IDLE_TIMEOUT_MILLIS ? RC.DATABASE.IDLE_TIMEOUT_MILLIS : 30000,
-
       // how often to check for idle resources to destroy
       reapIntervalMillis: RC.DATABASE.REAP_INTERVAL_MILLIS ? RC.DATABASE.REAP_INTERVAL_MILLIS : 1000,
-
       // long long to idle after failed create before trying again
       createRetryIntervalMillis: RC.DATABASE.CREATE_RETRY_INTERVAL_MILLIS ? RC.DATABASE.CREATE_RETRY_INTERVAL_MILLIS : 200
       // ping: function (conn, cb) { conn.query('SELECT 1', cb) }
