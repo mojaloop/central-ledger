a38ebee3c84553478936a51588c6ac17381e544c
Steven Oderayi <oderayi@gmail.com>
2019-11-19 15:11:39 +0100

    Add seed data for Participant DELETE endpoint types

--- seeds/endpointType.js
+++ seeds/endpointType.js
@@ -67,15 +67,27 @@ const endpointTypes = [
     description: 'Participant callback URL to which put participant information can be sent, when subId is specified'
   },
   {
-    name: 'FSPIOP_CALLBACK_URL_PARTIES_GET',
-    description: 'Participant callback URL to which get parties information can be requested'
-  },
-  {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_PUT_ERROR',
     description: 'Participant callback URL to which put participant error information can be sent'
   },
   {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_PUT_ERROR',
+    description: 'Participant callback URL to which put participant error information can be sent, when subId is specified'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_DELETE',
+    description: 'Participant callback URL to which delete participant information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_DELETE',
+    description: 'Participant callback URL to which delete participant information can be sent, when subId is specified'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_DELETE_ERROR',
+    description: 'Participant callback URL to which put participant error information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_DELETE_ERROR',
     description: 'Participant callback URL to which put participant error information can be sent, when subId is specified'
   },
   {
@@ -85,6 +97,10 @@ const endpointTypes = [
   {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_BATCH_PUT_ERROR',
     description: 'Participant callback URL to which put batch participant error information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTIES_GET',
+    description: 'Participant callback URL to which get parties information can be requested'
   },
   {
     name: 'FSPIOP_CALLBACK_URL_PARTIES_PUT',
