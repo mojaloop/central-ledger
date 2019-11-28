79ffb8eed04c5134a2c6812a1d16746ad2ead988
Steven Oderayi <oderayi@gmail.com>
2019-11-28 08:16:14 +0100

    Add seed data for new endpoint types (#736 & #1078) (#489)
    * Add seeds for participants SubId support
    
    * Update seed data for endpointTypes
    
    * Add seed data for Participant DELETE endpoint types
    
    * Remove uneccessary code

--- seeds/endpointType.js
+++ seeds/endpointType.js
@@ -19,6 +19,7 @@
  - Name Surname <name.surname@gatesfoundation.com>
 
  * Georgi Georgiev <georgi.georgiev@modusbox.com>
+
  --------------
  ******/
 
@@ -62,12 +63,24 @@ const endpointTypes = [
     description: 'Participant callback URL to which put participant information can be sent'
   },
   {
-    name: 'FSPIOP_CALLBACK_URL_PARTIES_GET',
-    description: 'Participant callback URL to which get parties information can be requested'
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_PUT',
+    description: 'Participant callback URL to which put participant information can be sent, when subId is specified'
   },
   {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_PUT_ERROR',
     description: 'Participant callback URL to which put participant error information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_PUT_ERROR',
+    description: 'Participant callback URL to which put participant error information can be sent, when subId is specified'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_DELETE',
+    description: 'Participant callback URL to which delete participant information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_SUB_ID_DELETE',
+    description: 'Participant callback URL to which delete participant information can be sent, when subId is specified'
   },
   {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_BATCH_PUT',
@@ -76,6 +89,10 @@ const endpointTypes = [
   {
     name: 'FSPIOP_CALLBACK_URL_PARTICIPANT_BATCH_PUT_ERROR',
     description: 'Participant callback URL to which put batch participant error information can be sent'
+  },
+  {
+    name: 'FSPIOP_CALLBACK_URL_PARTIES_GET',
+    description: 'Participant callback URL to which get parties information can be requested'
   },
   {
     name: 'FSPIOP_CALLBACK_URL_PARTIES_PUT',
