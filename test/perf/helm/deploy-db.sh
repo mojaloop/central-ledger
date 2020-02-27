2cf3b7c8a8dc5cda7603affe3badc349e41f27fc
Miguel de Barros <miguel@debarros.me>
2020-02-27 17:37:38 +0000

    updated perf db deploy script to add metrics annotations

--- test/perf/helm/deploy-db.sh
+++ test/perf/helm/deploy-db.sh
@@ -1,1 +1,6 @@
+## Helm installation of DB
 helm install --namespace testcss --name css-db stable/percona-xtradb-cluster -f ./valuesDb.yml
+
+## Add annotations for monitoring
+kubectl -n testcss annotate pods css-db-centralledger-mysql-0 prometheus.io/port=9104
+kubectl -n testcss annotate pods css-db-centralledger-mysql-0 prometheus.io/scrape=true
