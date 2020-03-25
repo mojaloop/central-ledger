c1bf9cac0b8fd60e9b6982d4ddd13e6047c7c655
Yosh Kemu <y@ke.mu>
2020-03-25 12:24:53 +0000

    Removed double UV_THREADPOOL_SIZE setting

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,6 @@ const Config = require('../lib/config')
 const Routes = require('./routes')
 const Setup = require('../shared/setup')
 
-process.env.UV_THREADPOOL_SIZE = 12
-
 module.exports = Setup.initialize({
   service: 'api',
   port: Config.PORT,
