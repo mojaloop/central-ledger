588d7d643f799ef248d4ece795a48ea7ec591712
Lazola Lucas <lazola.lucas@modusbox.com>
2020-06-08 12:28:50 +0200

    Merge remote-tracking branch 'mojaloop/master' into feature/OTC-390-Implement-getIlpPacket

---
+++ migrations/501001_quoteExtension-quote-service-174.js
@@ -0,0 +1,46 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * ModusBox
+ - James Bush <james.bush@modusbox.com>
+ --------------
+ ******/
+
+'use strict'
+
+exports.up = async (knex) => {
+
+  /**
+   * Make quoteResponseId column on quoteExtension table nullable.
+   * This change comes from quote-service issue #174
+   */
+  return await knex.schema.hasTable('quoteExtension').then(function(exists) {
+    if (exists) {
+      return knex.schema.alterTable('quoteExtension', (t) => {
+        t.bigInteger('quoteResponseId').unsigned().nullable().alter()
+        t.string('transactionId', 36).nullable().comment('The transaction reference that is part of the initial quote').alter()
+      })
+    }
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('quoteExtension')
+}
