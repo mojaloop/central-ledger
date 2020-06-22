0c8a12c9de474cf1e87a81b47c1e78ee195d613c
Sam <ksamuelbenny@gmail.com>
2020-06-22 18:14:10 +0530

    Update error message when Payer FSP and Payee FSP are the same and on-us is not enabled.. (Added text "FSP" to specify)

--- src/handlers/transfers/validator.js
+++ src/handlers/transfers/validator.js
@@ -89,7 +89,7 @@ const validateDifferentDfsp = (payload) => {
   if (!Config.ENABLE_ON_US_TRANSFERS) {
     const isPayerAndPayeeDifferent = (payload.payerFsp.toLowerCase() !== payload.payeeFsp.toLowerCase())
     if (!isPayerAndPayeeDifferent) {
-      reasons.push('Payer and Payee should be different')
+      reasons.push('Payer FSP and Payee FSP should be different, unless on-us tranfers are allowed by the Scheme')
       return false
     }
   }
