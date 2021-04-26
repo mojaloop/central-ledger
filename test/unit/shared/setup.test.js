b5d9d9775936292b587b5c180902f35481da65d9
vgenev <vgenev@gmail.com>
2021-04-26 18:18:51 +0300

    removed default settlement model from config seeder

--- test/unit/shared/setup.test.js
+++ test/unit/shared/setup.test.js
@@ -25,7 +25,6 @@ Test('setup', setupTest => {
   let HapiStub
   let UrlParserStub
   let serverStub
-  let ConfigDataSeederStub
   let processExitStub
   // let KafkaCronStub
 
@@ -114,9 +113,6 @@ Test('setup', setupTest => {
         registerBulkProcessingHandler: sandbox.stub().returns(Promise.resolve())
       }
     }
-    ConfigDataSeederStub = {
-      initializeSeedData: sandbox.stub().resolves()
-    }
     const ConfigStub = Config
     ConfigStub.HANDLERS_API_DISABLED = false
     ConfigStub.HANDLERS_CRON_DISABLED = false
@@ -134,8 +130,7 @@ Test('setup', setupTest => {
       './plugins': PluginsStub,
       '../lib/urlParser': UrlParserStub,
       '@hapi/hapi': HapiStub,
-      '../lib/config': ConfigStub,
-      '../lib/configDataSeeder': ConfigDataSeederStub
+      '../lib/config': ConfigStub
       // '../handlers/lib/kafka': KafkaCronStub
     })
 
@@ -178,8 +173,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStubThrowError,
-        '../lib/config': Config,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': Config
 
         // '../handlers/lib/kafka': KafkaCronStub
       })
@@ -231,9 +225,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': ConfigStub,
-        '../lib/configDataSeeder': ConfigDataSeederStub
-        // '../handlers/lib/kafka': KafkaCronStub
+        '../lib/config': ConfigStub
       })
 
       Setup.initialize({ service }).then(s => {
@@ -323,17 +315,6 @@ Test('setup', setupTest => {
       })
     })
 
-    initializeTest.test('initialize configurable seeded data', async (test) => {
-      const service = 'api'
-
-      Setup.initialize({ service, runMigrations: true }).then(() => {
-        test.ok(ConfigDataSeederStub.initializeSeedData.called)
-        test.end()
-      }).catch(err => {
-        test.fail(`Should have not received an error: ${err}`)
-        test.end()
-      })
-    })
     initializeTest.test('run Handlers if runHandlers flag enabled and start API', async (test) => {
       const service = 'handler'
 
@@ -360,8 +341,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': Config,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': Config
         // '../handlers/lib/kafka': KafkaCronStub
       })
 
@@ -393,8 +373,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': ConfigStub,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': ConfigStub
         // '../handlers/lib/kafka': KafkaCronStub
       })
 
@@ -427,8 +406,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': ConfigStub,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': ConfigStub
         // '../handlers/lib/kafka': KafkaCronStub
       })
 
@@ -463,8 +441,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': Config,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': Config
         // '../handlers/lib/kafka': KafkaCronStub
       })
 
@@ -697,8 +674,7 @@ Test('setup', setupTest => {
         './plugins': PluginsStub,
         '../lib/urlParser': UrlParserStub,
         '@hapi/hapi': HapiStub,
-        '../lib/config': Config,
-        '../lib/configDataSeeder': ConfigDataSeederStub
+        '../lib/config': Config
 
         // '../handlers/lib/kafka': KafkaCronStub
       })
