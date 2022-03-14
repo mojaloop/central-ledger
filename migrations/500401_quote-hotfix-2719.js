b9944d15c9486ffd62b968797fb79847a512a6c8
Miguel de Barros <miguel@debarros.me>
2022-03-14 15:53:47 +0200

    fix(mojaloop/#2719): post quotes fails when transactionId does not equal transactionRequestId (#887)
    fix(mojaloop/#2719): post quotes fails when transactionId does not equal transactionRequestId - https://github.com/mojaloop/project/issues/2719
    - added new migration script 500401_quote-2719.js to remove the transactionRequestId foreign key constraint that conflicts with transactionReferenceId foreign key constraint
    - updated dependencies
    - fixes for audit-resolve
    - sneaked in a fix for 910102_hotfix954.js as the revert script incorrectly dropped the entire table instead of just undoing the migration change

--- migrations/500401_quote-hotfix-2719.js
+++ migrations/500401_quote-hotfix-2719.js
@@ -5,7 +5,13 @@
  The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
  Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- Contributors
+
+ Initial contribution
+ --------------------
+ The initial functionality and code base was donated by the Mowali project working in conjunction with MTN and Orange as service provides.
+ * Project: Mowali
+
+Contributors
  --------------
  This is the official list of the Mojaloop project contributors for this file.
  Names of the original copyright holders (individuals or organizations)
@@ -23,18 +29,30 @@
  --------------
  ******/
 
+// Notes: this is a fix for the following issue: https://github.com/mojaloop/project/issues/2719
+
 'use strict'
 
-exports.up = async (knex, Promise) => {
-  return knex.schema.table('transferError', (t) => {
-    // Dropping foreign keys for the transferError table to allow the error to be logged without any record existing in the transferErrorDuplicateCheck
-    t.dropForeign('transferId')
+exports.up = (knex) => {
+  return knex.schema.hasTable('quote').then((exists) => {
+    if (exists) {
+      return knex.schema
+      .table('quote', (t) => {
+        // remove unnecessary foreign key constraint that has a conflict with transactionReferenceId foreign key constraint
+        t.dropForeign('transactionRequestId')
+      })
+    }
   })
 }
 
-exports.down = function (knex) {
-  return knex.schema.table('transferError', (t) => {
-    // Undo dropping of foreign key
-    t.foreign('transferId').references('transferId').inTable('transferErrorDuplicateCheck')
+exports.down = (knex) => {
+  return knex.schema.hasTable('quote').then((exists) => {
+    if (exists) {
+      return knex.schema
+      .table('quote', (t) => {
+        // lets reverse what we did
+        t.foreign('transactionRequestId').references('transactionReferenceId').inTable('transactionReference')
+      })
+    }
   })
 }
