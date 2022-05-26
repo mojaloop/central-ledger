def523564b0e143a3e53ba6d3e48632ea1889567
Miguel de Barros <miguel@debarros.me>
2022-05-26 13:20:29 +0200

    chore: fixes for test-integration ci job

--- test/integration/handlers/handlers.test.js
+++ test/integration/handlers/handlers.test.js
@@ -358,7 +358,15 @@ Test('Handlers test', async handlersTest => {
         TransferEventType.PREPARE.toUpperCase())
       prepareConfig.logger = Logger
       await Producer.produceMessage(td.messageProtocolPrepare, td.topicConfTransferPrepare, prepareConfig)
-      const transfer = await wrapWithRetries(() => TransferService.getById(td.messageProtocolPrepare.content.payload.transferId))
+      const transfer = await wrapWithRetries(async () => {
+        // lets fetch the transfer
+        const transfer = await TransferService.getById(td.messageProtocolPrepare.content.payload.transferId)
+        // lets check its status, and if its what we expect return the result
+        if (transfer.transferState === 'RESERVED') return transfer
+        // otherwise lets return nothing
+        return null
+      })
+
       test.equal(transfer.transferState, 'RESERVED', 'Transfer is in reserved state')
 
       // 2. send an ABORTED request from Payee
