08d066403ac5d2c625cd471ad9ad4283596057d0
Vijay Kumar <vijaya.guthi@modusbox.com>
2022-06-15 20:58:06 +0530

    Removed docker-local folder

--- docker-local/docker-clean-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
--- docker-local/docker/sql-als-init/01_permissions.sql
+++
@@ -1,2 +0,0 @@
-ALTER USER 'account_lookup'@'%' identified WITH mysql_native_password by 'password';
-FLUSH PRIVILEGES;
