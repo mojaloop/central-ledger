49eab0c756c3771b760dc56ae49031799eec8e4b
Kevin <kleyow@gmail.com>
2022-11-17 18:54:17 -0500

    chore: add warning when mongodb is disabled

--- src/shared/setup.js
+++ src/shared/setup.js
@@ -78,6 +78,7 @@ const connectMongoose = async () => {
       // return null
     }
   } else {
+    Logger.isWarnEnabled && Logger.warn('Mongo Database is disabled in configuration')
     return null
   }
 }
