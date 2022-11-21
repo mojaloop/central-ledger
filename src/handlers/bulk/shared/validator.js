efa5efca257eb908156d5026d7122d7e3aa3df1c
Miguel de Barros <miguel@debarros.me>
2022-11-21 15:47:29 +0200

    Update src/handlers/bulk/shared/validator.js
    Co-authored-by: Sam <10507686+elnyry-sam-k@users.noreply.github.com>

--- src/handlers/bulk/shared/validator.js
+++ src/handlers/bulk/shared/validator.js
@@ -93,7 +93,7 @@ const validateFspiopSourceAndDestination = async (payload, headers) => {
       // The following was added for fix(mojaloop/##3024): [core bulk] POST /bulkTransfers from Switch --> PayeeFSP to use Switch as fspiop-source - https://github.com/mojaloop/project/issues/3024
       // Notes:
       //  Due to the Bulk [Design Considerations](https://docs.mojaloop.io/technical/central-bulk-transfers/#_2-design-considerations),
-      //  it is possible that the Switch may send a POST Request to the Payer with the Source Header containing "Switch",
+      //  it is possible that the Switch may send a POST Request to the Payee FSP with the Source Header containing "Switch",
       //  and the Payee FSP thus responding with a PUT Callback and destination header containing the same value (Switch).
       (headers[Enum.Http.Headers.FSPIOP.DESTINATION] === Enum.Http.Headers.FSPIOP.SWITCH.value)
     )
