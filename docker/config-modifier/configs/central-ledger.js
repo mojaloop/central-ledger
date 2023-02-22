d2d921b92195a83125ad41eab5a31cc1f220280c
Kevin <kleyow@gmail.com>
2023-02-22 17:10:18 -0500

    chore: config

--- docker/config-modifier/configs/central-ledger.js
+++ docker/config-modifier/configs/central-ledger.js
@@ -1,4 +1,8 @@
 module.exports = {
+  HOSTNAME: 'http://central-ledger',
+  DATABASE: {
+    HOST: 'mysql'
+  },
   MONGODB: {
     DISABLED: false,
     DEBUG: false,
@@ -7,5 +11,130 @@ module.exports = {
     USER: '',
     PASSWORD: '',
     DATABASE: 'mlos'
+  },
+  KAFKA: {
+    CONSUMER: {
+      BULK: {
+        PREPARE: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        PROCESSING: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        FULFIL: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        GET: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      },
+      TRANSFER: {
+        PREPARE: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        GET: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        FULFIL: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        POSITION: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      },
+      ADMIN: {
+        TRANSFER: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      }
+    },
+    PRODUCER: {
+      BULK: {
+        PROCESSING: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      },
+      TRANSFER: {
+        PREPARE: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        FULFIL: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        },
+        POSITION: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      },
+      NOTIFICATION: {
+        EVENT: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      },
+      ADMIN: {
+        TRANSFER: {
+          config: {
+            rdkafkaConf: {
+              'metadata.broker.list': 'kafka:29092'
+            }
+          }
+        }
+      }
+    }
   }
 }
