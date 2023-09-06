8da550a4cf3bd82dc6903a9883c3e74d4b182c5a
Steven Oderayi <oderayi@gmail.com>
2023-09-06 11:27:30 +0100

    chore(mojaloop/#3518): update database lib (#967)
    chore(mojaloop/#3518):update database lib - https://github.com/mojaloop/project/issues/3518
    - replaced `central-services-database` with `database-lib`

--- test/integration/handlers/root.test.js
+++ test/integration/handlers/root.test.js
@@ -27,7 +27,7 @@
 const Test = require('tape')
 const Joi = require('joi')
 const Logger = require('@mojaloop/central-services-logger')
-const Db = require('@mojaloop/central-services-database').Db
+const Db = require('@mojaloop/database-lib').Db
 
 const Config = require('../../../src/lib/config')
 const Consumer = require('@mojaloop/central-services-stream').Util.Consumer
