3d3a0c59292bf77bfa9d1e3951fd3adb5efa74f8
Kevin Leyow <kleyow@gmail.com>
2023-09-10 23:49:55 -0500

    chore: changes

--- src/domain/position/prepare.js
+++ src/domain/position/prepare.js
@@ -3,6 +3,7 @@ const ErrorHandler = require('@mojaloop/central-services-error-handling')
 const Config = require('../../lib/config')
 const Utility = require('@mojaloop/central-services-shared').Util
 const resourceVersions = require('@mojaloop/central-services-shared').Util.resourceVersions
+const MLNumber = require('@mojaloop/ml-number')
 
 /**
  * @function processPositionPrepareBin
@@ -20,27 +21,73 @@ const resourceVersions = require('@mojaloop/central-services-shared').Util.resou
  *
  * @returns {object} - Returns an object containing  accumulatedPositionValue, accumulatedPositionReservedValue, accumulatedTransferStateChanges, resultMessages or throws an error if failed
  */
-const processPositionPrepareBin = async (messages, accumulatedPositionValue, accumulatedPositionReservedValue, settlementPositionValue, settlementModelDelay, participantLimitValue, accumulatedTransferStateChanges) => {
+const processPositionPrepareBin = async (
+  messages,
+  initialTransferStateChangeList,
+  accumulatedPositionValue,
+  accumulatedPositionReservedValue,
+  settlementPositionValue,
+  settlementModelDelay,
+  participantLimit,
+  participantLimitValue,
+  accumulatedTransferStateChanges
+) => {
   let availablePosition
   const transferStateChanges = []
   const participantPositionChanges = []
   const resultMessages = []
-  const effectivePosition = accumulatedPositionValue + accumulatedPositionReservedValue
+  const limitAlarms = []
+  const effectivePosition = new MLNumber(accumulatedPositionValue + accumulatedPositionReservedValue)
+  const liquidityCover = new MLNumber(settlementPositionValue).multiply(-1)
 
-  if (settlementModelDelay === 'IMMEDIATE') {
-    availablePosition = settlementPositionValue + participantLimitValue - effectivePosition
+  // Enum.Settlements.SettlementDelay.IMMEDIATE or SettlementDelayName.IMMEDIATE. 0/1 or string?
+  if (settlementModelDelay === Enum.Settlements.SettlementDelay.IMMEDIATE) {
+    availablePosition = new MLNumber(settlementPositionValue + participantLimitValue - effectivePosition)
   } else {
-    availablePosition = participantLimitValue - effectivePosition
+    availablePosition = new MLNumber(participantLimitValue - effectivePosition)
   }
 
   for (const message of messages) {
-    // object key is transfer id, transfer state change
-    // Validate current state (transferStateChange.transferStateId == 'RECEIVED_PREPARE')
-    // Is this accumulatedTransferStateChanges? Am I to check the same index as the messages array?
     let transferStateId
     let reason
     let resultMessage
-    if (availablePosition >= message.payload.amount.amount) {
+
+    if (initialTransferStateChangeList[message.transferId] !== Enum.Transfers.TransferInternalState.RECEIVED_PREPARE) {
+      transferStateId = Enum.Transfers.TransferState.ABORTED_REJECTED
+      reason = 'Transfer in incorrect state'
+
+      const headers = Utility.Http.SwitchDefaultHeaders(
+        message.payerFsp,
+        Enum.Http.HeaderResources.TRANSFERS,
+        Enum.Http.Headers.FSPIOP.SWITCH.value,
+        resourceVersions[Enum.Http.HeaderResources.TRANSFERS].contentVersion
+      )
+      const fspiopError = ErrorHandler.Factory.createFSPIOPError(
+        ErrorHandler.Enums.FSPIOPErrorCodes.INTERNAL_SERVER_ERROR
+      ).toApiErrorObject(Config.ERROR_HANDLING)
+      const state = Utility.StreamingProtocol.createEventState(
+        Enum.Events.EventStatus.FAILURE.status,
+        fspiopError.errorInformation.errorCode,
+        fspiopError.errorInformation.errorDescription
+      )
+      const metadata = Utility.StreamingProtocol.createMetadataWithCorrelatedEvent(
+        message.transferId,
+        Enum.Kafka.Topics.NOTIFICATION,
+        Enum.Events.Event.Action.PREPARE,
+        state
+      )
+
+      resultMessage = Utility.StreamingProtocol.createMessage(
+        message.transferId,
+        message.payeeFsp,
+        message.payerFsp,
+        metadata,
+        headers,
+        fspiopError,
+        { id: message.transferId },
+        'application/json'
+      )
+    } else if (availablePosition >= message.payload.amount.amount) {
       transferStateId = Enum.Transfers.TransferState.RESERVED
       availablePosition = availablePosition - message.payload.amount.amount
 
@@ -103,6 +150,7 @@ const processPositionPrepareBin = async (messages, accumulatedPositionValue, acc
         'application/json'
       )
     }
+
     resultMessages.push(resultMessage)
 
     const transferStateChange = {
@@ -118,14 +166,17 @@ const processPositionPrepareBin = async (messages, accumulatedPositionValue, acc
     }
     participantPositionChanges.push(participantPositionChange)
 
-    // Look @ prepare 8 Assess any limit thresholds on the position adding to alarm list if triggered
+    if (availablePosition.toNumber() > liquidityCover.multiply(participantLimit.thresholdAlarmPercentage).toNumber()) {
+      limitAlarms.push(participantLimit)
+    }
   }
   return {
     accumulatedPosition: availablePosition,
     transferStateChanges,
     participantPositionChanges,
     resultMessages,
-    accumulatedTransferStateChanges
+    accumulatedTransferStateChanges,
+    limitAlarms
   }
 }
 
