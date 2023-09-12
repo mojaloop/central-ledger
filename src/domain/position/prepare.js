798ac7e6608de7e91a9f4b98f5ef123fbd09d046
Kevin Leyow <kleyow@gmail.com>
2023-09-11 19:48:32 -0500

    chore: unit tests

--- src/domain/position/prepare.js
+++ src/domain/position/prepare.js
@@ -7,6 +7,8 @@ const MLNumber = require('@mojaloop/ml-number')
 const SettlementModelCached = require('../../models/settlement/settlementModelCached')
 const ParticipantFacade = require('../../models/participant/facade')
 const BatchModel = require('../../models/position/batch')
+const Logger = require('@mojaloop/central-services-logger')
+
 /**
  * @function processPositionPrepareBin
  *
@@ -33,6 +35,7 @@ const processPositionPrepareBin = async (
   const limitAlarms = []
   const accumulatedTransferStateCopy = Object.assign({}, accumulatedTransferState)
   const effectivePosition = new MLNumber(accumulatedPositionValue + accumulatedPositionReservedValue)
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::effectivePosition: ${effectivePosition}`)
 
   // Bin items should be grouped by participant currency id so lets use the first one
   const participantName = binItems[0].message.payload.payerFsp
@@ -44,15 +47,20 @@ const processPositionPrepareBin = async (
     currencyId,
     Enum.Accounts.LedgerAccountType.POSITION
   )
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::participantCurrency: ${JSON.stringify(participantCurrency)}`)
+
   const participantLimit = await ParticipantFacade.getParticipantLimitByParticipantCurrencyLimit(
     participantCurrency.participantId,
     participantCurrency.currencyId,
     Enum.Accounts.LedgerAccountType.POSITION,
     Enum.Accounts.ParticipantLimitType.NET_DEBIT_CAP
   )
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::participantLimit: ${JSON.stringify(participantLimit)}`)
 
   // Query the settlement models to get the settlement delay
   const allSettlementModels = await SettlementModelCached.getAll()
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::allSettlementModels: ${JSON.stringify(allSettlementModels)}`)
+
   let settlementModels = allSettlementModels.filter(model => model.currencyId === currencyId)
   if (settlementModels.length === 0) {
     settlementModels = allSettlementModels.filter(model => model.currencyId === null) // Default settlement model
@@ -61,20 +69,28 @@ const processPositionPrepareBin = async (
     }
   }
   const settlementModel = settlementModels.find(sm => sm.ledgerAccountTypeId === Enum.Accounts.LedgerAccountType.POSITION)
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::settlementModel: ${JSON.stringify(settlementModel)}`)
+
   const settlementParticipantCurrency = await ParticipantFacade.getByNameAndCurrency(participantName, currencyId, settlementModel.settlementAccountTypeId)
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::settlementParticipantCurrency: ${JSON.stringify(settlementParticipantCurrency)}`)
 
   const participantPositions = await BatchModel.getPositionsByAccountIdsNonTrx(
     [participantCurrency.participantCurrencyId, settlementParticipantCurrency.participantCurrencyId]
   )
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::participantPositions: ${JSON.stringify(participantPositions)}`)
 
   const settlementParticipantPosition = participantPositions[settlementParticipantCurrency.participantCurrencyId]
+  Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::settlementParticipantPosition: ${settlementParticipantPosition}`)
+
   const liquidityCover = new MLNumber(settlementParticipantPosition).multiply(-1)
 
   // Calculate the available position, how much funds the payer participant can move, based on the settlement model delay
   if (settlementModel.settlementDelayId === Enum.Settlements.SettlementDelay.IMMEDIATE) {
     availablePosition = new MLNumber(settlementParticipantPosition + participantLimit.value - effectivePosition)
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::availablePosition: ${availablePosition} = ${settlementParticipantPosition} + ${participantLimit.value} - ${effectivePosition}`)
   } else {
     availablePosition = new MLNumber(participantLimit.value - effectivePosition)
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::availablePosition: ${availablePosition} = ${participantLimit.value} - ${effectivePosition}`)
   }
 
   for (const binItem of binItems) {
@@ -82,9 +98,13 @@ const processPositionPrepareBin = async (
     let reason
     let resultMessage
     const transfer = binItem.message.payload
+    Logger.isDebugEnabled && Logger.debug('-'.repeat(100))
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::transfer:processingMessage: ${JSON.stringify(transfer)}`)
 
     // Check if transfer is in correct state for processing, produce an internal error message
     if (accumulatedTransferState[transfer.transferId] !== Enum.Transfers.TransferInternalState.RECEIVED_PREPARE) {
+      Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::transferState: ${accumulatedTransferState[transfer.transferId]} !== ${Enum.Transfers.TransferInternalState.RECEIVED_PREPARE}`)
+
       transferStateId = Enum.Transfers.TransferInternalState.ABORTED_REJECTED
       reason = 'Transfer in incorrect state'
 
@@ -120,11 +140,19 @@ const processPositionPrepareBin = async (
         'application/json'
       )
     // Check if payer participant has sufficient liquidity to process the transfer, produce success message
-    } else if (availablePosition >= transfer.amount.amount) {
+    } else if (availablePosition.toNumber() >= transfer.amount.amount) {
+      Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::availablePosition: ${availablePosition} >= ${transfer.amount.amount}`)
+
       transferStateId = Enum.Transfers.TransferState.RESERVED
       availablePosition = availablePosition.subtract(transfer.amount.amount)
 
-      const headers = null // ?
+      // Are these the right headers?
+      const headers = Utility.Http.SwitchDefaultHeaders(
+        transfer.payeeFsp,
+        Enum.Http.HeaderResources.TRANSFERS,
+        transfer.payerFsp,
+        resourceVersions[Enum.Http.HeaderResources.TRANSFERS].contentVersion
+      )
       const state = Utility.StreamingProtocol.createEventState(
         Enum.Events.EventStatus.SUCCESS.status,
         null,
@@ -188,10 +216,12 @@ const processPositionPrepareBin = async (
     resultMessages.push({ binItem, message: resultMessage })
 
     const transferStateChange = {
+      transferId: transfer.transferId, //
       transferStateId,
       reason
     }
     transferStateChanges.push(transferStateChange)
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::transferStateChange: ${JSON.stringify(transferStateChange)}`)
 
     const participantPositionChange = {
       transferStateChangeId: null, // Need to update this in bin processor while executing queries
@@ -199,21 +229,26 @@ const processPositionPrepareBin = async (
       reservedValue: accumulatedPositionReservedValue
     }
     participantPositionChanges.push(participantPositionChange)
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::participantPositionChange: ${JSON.stringify(participantPositionChange)}`)
 
+    // Logic here seems to be faulty.
     if (availablePosition.toNumber() > liquidityCover.multiply(participantLimit.thresholdAlarmPercentage).toNumber()) {
+      Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::availablePosition: ${availablePosition} > ${liquidityCover.multiply(participantLimit.thresholdAlarmPercentage)}`)
       limitAlarms.push(participantLimit)
     }
 
     accumulatedTransferStateCopy[transfer.transferId] = transferStateId
+    Logger.isDebugEnabled && Logger.debug(`processPositionPrepareBin::accumulatedTransferStateCopy:finalizedTransferState ${JSON.stringify(transferStateId)}`)
+    Logger.isDebugEnabled && Logger.debug('-'.repeat(100))
   }
 
   return {
     accumulatedPosition: availablePosition.toNumber(),
-    participantPositionChanges,
-    resultMessages,
-    accumulatedTransferStateChanges: transferStateChanges,
-    accumulatedTransferState: accumulatedTransferStateCopy,
-    limitAlarms
+    accumulatedTransferState: accumulatedTransferStateCopy, // finalizes transfer state after prepare processing
+    accumulatedTransferStateChanges: transferStateChanges, // transfer state changes to be persisted in order
+    limitAlarms, // array of participant limits that have been breached
+    participantPositionChanges, // participant position changes to be persisted in order
+    resultMessages // array of objects containing bin item and result message. {binItem, message}
   }
 }
 
--- test/unit/domain/position/prepare.test.js
+++ test/unit/domain/position/prepare.test.js
@@ -119,6 +119,8 @@ Test('Prepare domain', positionIndexTest => {
         participantLimitId: 1,
         thresholdAlarmPercentage: 0.5
       }
+      // Not sure of what should be the correctly structured model here.
+      // All I know is that the model should have the following properties: currencyId, the right settlementAccountId
       const allSettlementModels = [{
         settlementModelId: 1,
         name: 'DEFERREDNET',
@@ -144,8 +146,8 @@ Test('Prepare domain', positionIndexTest => {
 
       const processedMessages = await processPositionPrepareBin(
         binItems,
-        1,
-        1,
+        0,
+        0,
         {
           'b51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
           'c51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
@@ -154,6 +156,20 @@ Test('Prepare domain', positionIndexTest => {
       )
       Logger.isInfoEnabled && Logger.info(processedMessages)
       test.equal(processedMessages.resultMessages.length, 3)
+      test.equal(processedMessages.resultMessages[0].message.content.uriParams.id, 'b51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[0].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Destination'], 'dfsp2')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.accumulatedTransferState[transferMessage1.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
+      test.equal(processedMessages.resultMessages[1].message.content.uriParams.id, 'c51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[1].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Destination'], 'dfsp3')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.accumulatedTransferState[transferMessage2.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
       test.equal(processedMessages.resultMessages[2].message.content.uriParams.id, 'd51ec534-ee48-4575-b6a9-ead2955b8999')
       test.equal(processedMessages.resultMessages[2].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
       test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Destination'], 'dfsp1')
@@ -161,7 +177,220 @@ Test('Prepare domain', positionIndexTest => {
       test.equal(processedMessages.resultMessages[2].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
       test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorCode, '2001')
       test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorDescription, 'Internal server error')
+      test.equal(processedMessages.accumulatedTransferState[transferMessage3.payload.transferId], Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferId, transferMessage1.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferId, transferMessage2.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferId, transferMessage3.payload.transferId)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferStateId, Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedPosition, 9800)
       test.end()
+    })
+
+    changeParticipantPositionTest.test('produce abort message for when payer does not have enough liquidity', async (test) => {
+      const participant = {
+        participantId: 0,
+        name: 'payerFsp',
+        currency: 'USD',
+        isActive: 1,
+        createdDate: new Date(),
+        participantCurrencyId: 1
+      }
+      const settlementAccount = {
+        participantCurrencyId: 2
+      }
+      const participantLimit = {
+        participantCurrencyId: 1,
+        participantLimitTypeId: 1,
+        value: 10, // Set low
+        isActive: 1,
+        createdBy: 'unknown',
+        participantLimitId: 1,
+        thresholdAlarmPercentage: 0.5
+      }
+      // Not sure of what should be the correctly structured model here.
+      // All I know is that the model should have the following properties: currencyId, the right settlementAccountId
+      const allSettlementModels = [{
+        settlementModelId: 1,
+        name: 'DEFERREDNET',
+        isActive: 1,
+        settlementGranularityId: 2,
+        settlementInterchangeId: 2,
+        settlementDelayId: 2,
+        currencyId: 'USD',
+        requireLiquidityCheck: 1,
+        ledgerAccountTypeId: 1,
+        autoPositionReset: 1,
+        adjustPosition: 0,
+        settlementAccountTypeId: 2 // Is this Enum.Accounts.LedgerAccountType.POSITION?
+      }]
+
+      SettlementModelCached.getAll.returns(allSettlementModels)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 1).returns(participant)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 2).returns(settlementAccount)
+      ParticipantFacade.getParticipantLimitByParticipantCurrencyLimit.returns(Promise.resolve(participantLimit))
+      BatchModel.getPositionsByAccountIdsNonTrx.returns({
+        1: 10 // Set low
+      })
+
+      const processedMessages = await processPositionPrepareBin(
+        binItems,
+        0,
+        0,
+        {
+          'b51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'c51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'd51ec534-ee48-4575-b6a9-ead2955b8999': 'INVALID_STATE'
+        }
+      )
+      Logger.isInfoEnabled && Logger.info(processedMessages)
+      test.equal(processedMessages.resultMessages.length, 3)
+
+      test.equal(processedMessages.resultMessages[0].message.content.uriParams.id, 'b51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[0].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Destination'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Source'], 'switch')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[0].message.content.payload.errorInformation.errorCode, '4001')
+      test.equal(processedMessages.resultMessages[0].message.content.payload.errorInformation.errorDescription, 'Payer FSP insufficient liquidity')
+      test.equal(processedMessages.participantPositionChanges[0].value, 10)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage1.payload.transferId], Enum.Transfers.TransferState.ABORTED)
+
+      test.equal(processedMessages.resultMessages[1].message.content.uriParams.id, 'c51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[1].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Destination'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Source'], 'switch')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[1].message.content.payload.errorInformation.errorCode, '4001')
+      test.equal(processedMessages.resultMessages[1].message.content.payload.errorInformation.errorDescription, 'Payer FSP insufficient liquidity')
+      test.equal(processedMessages.participantPositionChanges[1].value, 10)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage2.payload.transferId], Enum.Transfers.TransferState.ABORTED)
+
+      test.equal(processedMessages.resultMessages[2].message.content.uriParams.id, 'd51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[2].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Destination'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Source'], 'switch')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorCode, '2001')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorDescription, 'Internal server error')
+      test.equal(processedMessages.participantPositionChanges[2].value, 10)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage3.payload.transferId], Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferId, transferMessage1.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferId, transferMessage2.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferId, transferMessage3.payload.transferId)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferStateId, Enum.Transfers.TransferState.ABORTED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferStateId, Enum.Transfers.TransferState.ABORTED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferStateId, Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedPosition, 10)
+      test.end()
+    })
+
+    changeParticipantPositionTest.test('produce abort message reserved messages for valid transfer messages', async (test) => {
+      const participant = {
+        participantId: 0,
+        name: 'payerFsp',
+        currency: 'USD',
+        isActive: 1,
+        createdDate: new Date(),
+        participantCurrencyId: 1
+      }
+      const settlementAccount = {
+        participantCurrencyId: 2
+      }
+      const participantLimit = {
+        participantCurrencyId: 1,
+        participantLimitTypeId: 1,
+        value: 10000,
+        isActive: 1,
+        createdBy: 'unknown',
+        participantLimitId: 1,
+        thresholdAlarmPercentage: 0.5
+      }
+      // Not sure of what should be the correctly structured model here.
+      // All I know is that the model should have the following properties: currencyId, the right settlementAccountId
+      const allSettlementModels = [{
+        settlementModelId: 1,
+        name: 'DEFERREDNET',
+        isActive: 1,
+        settlementGranularityId: 2,
+        settlementInterchangeId: 2,
+        settlementDelayId: 2,
+        currencyId: 'USD',
+        requireLiquidityCheck: 1,
+        ledgerAccountTypeId: 1,
+        autoPositionReset: 1,
+        adjustPosition: 0,
+        settlementAccountTypeId: 2 // Is this Enum.Accounts.LedgerAccountType.POSITION?
+      }]
+
+      SettlementModelCached.getAll.returns(allSettlementModels)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 1).returns(participant)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 2).returns(settlementAccount)
+      ParticipantFacade.getParticipantLimitByParticipantCurrencyLimit.returns(Promise.resolve(participantLimit))
+      BatchModel.getPositionsByAccountIdsNonTrx.returns({
+        1: 100000,
+        2: 100000
+      })
+
+      const processedMessages = await processPositionPrepareBin(
+        binItems,
+        0,
+        0,
+        {
+          'b51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'c51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'd51ec534-ee48-4575-b6a9-ead2955b8999': 'INVALID_STATE'
+        }
+      )
+      Logger.isInfoEnabled && Logger.info(processedMessages)
+      test.equal(processedMessages.resultMessages.length, 3)
+
+      test.equal(processedMessages.resultMessages[0].message.content.uriParams.id, 'b51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[0].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Destination'], 'dfsp2')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.participantPositionChanges[0].value, 9900)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage1.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
+      test.equal(processedMessages.resultMessages[1].message.content.uriParams.id, 'c51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[1].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Destination'], 'dfsp3')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.participantPositionChanges[1].value, 9800)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage2.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
+      test.equal(processedMessages.resultMessages[2].message.content.uriParams.id, 'd51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[2].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Destination'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Source'], 'switch')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorCode, '2001')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorDescription, 'Internal server error')
+      test.equal(processedMessages.participantPositionChanges[2].value, 9800)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage3.payload.transferId], Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferId, transferMessage1.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferId, transferMessage2.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferId, transferMessage3.payload.transferId)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferStateId, Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedPosition, 9800)
+      test.end()
+    })
+
+    changeParticipantPositionTest.skip('produce proper limit alarms', async (test) => {
     })
 
     changeParticipantPositionTest.end()
