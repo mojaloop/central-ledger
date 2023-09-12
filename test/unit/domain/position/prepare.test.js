f36ba0bb68a46ef9f01795c10f15aaa4f5aa5e9c
Kevin Leyow <kleyow@gmail.com>
2023-09-12 10:51:57 -0500

    chore: test

--- test/unit/domain/position/prepare.test.js
+++ test/unit/domain/position/prepare.test.js
@@ -127,10 +127,10 @@ Test('Prepare domain', positionIndexTest => {
         isActive: 1,
         settlementGranularityId: 2,
         settlementInterchangeId: 2,
-        settlementDelayId: 2,
+        settlementDelayId: 2, // 1 Immediate, 2 Deferred
         currencyId: 'USD',
         requireLiquidityCheck: 1,
-        ledgerAccountTypeId: 1,
+        ledgerAccountTypeId: 1, // 1 Position, 2 Settlement
         autoPositionReset: 1,
         adjustPosition: 0,
         settlementAccountTypeId: 2
@@ -220,13 +220,13 @@ Test('Prepare domain', positionIndexTest => {
         isActive: 1,
         settlementGranularityId: 2,
         settlementInterchangeId: 2,
-        settlementDelayId: 2,
+        settlementDelayId: 2, // 1 Immediate, 2 Deferred
         currencyId: 'USD',
         requireLiquidityCheck: 1,
-        ledgerAccountTypeId: 1,
+        ledgerAccountTypeId: 1, // 1 Position, 2 Settlement
         autoPositionReset: 1,
         adjustPosition: 0,
-        settlementAccountTypeId: 2 // Is this Enum.Accounts.LedgerAccountType.POSITION?
+        settlementAccountTypeId: 2
       }]
 
       SettlementModelCached.getAll.returns(allSettlementModels)
@@ -321,13 +321,13 @@ Test('Prepare domain', positionIndexTest => {
         isActive: 1,
         settlementGranularityId: 2,
         settlementInterchangeId: 2,
-        settlementDelayId: 2,
+        settlementDelayId: 2, // 1 Immediate, 2 Deferred
         currencyId: 'USD',
         requireLiquidityCheck: 1,
-        ledgerAccountTypeId: 1,
+        ledgerAccountTypeId: 1, // 1 Position, 2 Settlement
         autoPositionReset: 1,
         adjustPosition: 0,
-        settlementAccountTypeId: 2 // Is this Enum.Accounts.LedgerAccountType.POSITION?
+        settlementAccountTypeId: 2
       }]
 
       SettlementModelCached.getAll.returns(allSettlementModels)
@@ -387,6 +387,154 @@ Test('Prepare domain', positionIndexTest => {
       test.equal(processedMessages.accumulatedTransferStateChanges[2].transferStateId, Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
 
       test.equal(processedMessages.accumulatedPosition, 9800)
+      test.end()
+    })
+
+    changeParticipantPositionTest.test('include settlementParticipantPosition in availablePosition calc when settlement model delay is IMMEDIATE', async (test) => {
+      const participant = {
+        participantId: 0,
+        name: 'payerFsp',
+        currency: 'USD',
+        isActive: 1,
+        createdDate: new Date(),
+        participantCurrencyId: 1
+      }
+      const settlementAccount = {
+        participantCurrencyId: 2
+      }
+      const participantLimit = {
+        participantCurrencyId: 1,
+        participantLimitTypeId: 1,
+        value: 10000,
+        isActive: 1,
+        createdBy: 'unknown',
+        participantLimitId: 1,
+        thresholdAlarmPercentage: 0.5
+      }
+      // Not sure of what should be the correctly structured model here.
+      // All I know is that the model should have the following properties: currencyId, the right settlementAccountId
+      const allSettlementModels = [{
+        settlementModelId: 1,
+        name: 'DEFERREDNET',
+        isActive: 1,
+        settlementGranularityId: 2,
+        settlementInterchangeId: 2,
+        settlementDelayId: 1, // 1 Immediate, 2 Deferred
+        currencyId: 'USD',
+        requireLiquidityCheck: 1,
+        ledgerAccountTypeId: 1, // 1 Position, 2 Settlement
+        autoPositionReset: 1,
+        adjustPosition: 0,
+        settlementAccountTypeId: 2
+      }]
+
+      SettlementModelCached.getAll.returns(allSettlementModels)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 1).returns(participant)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 2).returns(settlementAccount)
+      ParticipantFacade.getParticipantLimitByParticipantCurrencyLimit.returns(Promise.resolve(participantLimit))
+      BatchModel.getPositionsByAccountIdsNonTrx.returns({
+        1: 100000,
+        2: 100000
+      })
+
+      const processedMessages = await processPositionPrepareBin(
+        binItems,
+        0,
+        0,
+        {
+          'b51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'c51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+          'd51ec534-ee48-4575-b6a9-ead2955b8999': 'INVALID_STATE'
+        }
+      )
+      Logger.isInfoEnabled && Logger.info(processedMessages)
+      test.equal(processedMessages.resultMessages.length, 3)
+
+      test.equal(processedMessages.resultMessages[0].message.content.uriParams.id, 'b51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[0].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Destination'], 'dfsp2')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[0].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.participantPositionChanges[0].value, 109900)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage1.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
+      test.equal(processedMessages.resultMessages[1].message.content.uriParams.id, 'c51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[1].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Destination'], 'dfsp3')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['FSPIOP-Source'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[1].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.participantPositionChanges[1].value, 109800)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage2.payload.transferId], Enum.Transfers.TransferState.RESERVED)
+
+      test.equal(processedMessages.resultMessages[2].message.content.uriParams.id, 'd51ec534-ee48-4575-b6a9-ead2955b8999')
+      test.equal(processedMessages.resultMessages[2].message.content.headers.accept, 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Destination'], 'dfsp1')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['FSPIOP-Source'], 'switch')
+      test.equal(processedMessages.resultMessages[2].message.content.headers['Content-Type'], 'application/vnd.interoperability.transfers+json;version=1.0')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorCode, '2001')
+      test.equal(processedMessages.resultMessages[2].message.content.payload.errorInformation.errorDescription, 'Internal server error')
+      test.equal(processedMessages.participantPositionChanges[2].value, 109800)
+      test.equal(processedMessages.accumulatedTransferState[transferMessage3.payload.transferId], Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferId, transferMessage1.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferId, transferMessage2.payload.transferId)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferId, transferMessage3.payload.transferId)
+
+      test.equal(processedMessages.accumulatedTransferStateChanges[0].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[1].transferStateId, Enum.Transfers.TransferState.RESERVED)
+      test.equal(processedMessages.accumulatedTransferStateChanges[2].transferStateId, Enum.Transfers.TransferInternalState.ABORTED_REJECTED)
+
+      test.equal(processedMessages.accumulatedPosition, 109800)
+      test.end()
+    })
+
+    changeParticipantPositionTest.test('produce error if no settlement models found', async (test) => {
+      const participant = {
+        participantId: 0,
+        name: 'payerFsp',
+        currency: 'USD',
+        isActive: 1,
+        createdDate: new Date(),
+        participantCurrencyId: 1
+      }
+      const settlementAccount = {
+        participantCurrencyId: 2
+      }
+      const participantLimit = {
+        participantCurrencyId: 1,
+        participantLimitTypeId: 1,
+        value: 10000,
+        isActive: 1,
+        createdBy: 'unknown',
+        participantLimitId: 1,
+        thresholdAlarmPercentage: 0.5
+      }
+      const allSettlementModels = []
+
+      SettlementModelCached.getAll.returns(allSettlementModels)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 1).returns(participant)
+      ParticipantFacade.getByNameAndCurrency.withArgs(payerFsp, currency, 2).returns(settlementAccount)
+      ParticipantFacade.getParticipantLimitByParticipantCurrencyLimit.returns(Promise.resolve(participantLimit))
+      BatchModel.getPositionsByAccountIdsNonTrx.returns({
+        1: 100000,
+        2: 100000
+      })
+      try {
+        await processPositionPrepareBin(
+          binItems,
+          0,
+          0,
+          {
+            'b51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+            'c51ec534-ee48-4575-b6a9-ead2955b8999': Enum.Transfers.TransferInternalState.RECEIVED_PREPARE,
+            'd51ec534-ee48-4575-b6a9-ead2955b8999': 'INVALID_STATE'
+          }
+        )
+        test.notOk('Should throw error')
+      } catch (error) {
+        test.ok(error)
+      }
+
       test.end()
     })
 
