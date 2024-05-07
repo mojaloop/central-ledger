5988eae1327dd592fefeb7d949ae44c23a553c20
Eugen Klymniuk <keramet.kh@gmail.com>
2024-05-07 12:45:30 +0100

    feat(mojaloop/#3818): added sequence and ER diagrams for transfer/fxTransfer flows (#1029)
    * feat(mojaloop/#3818): added sequence and ER diagrams for transfer timeout cron
    
    * feat(mojaloop/#3818): added sequence diagram for FX timeout
    
    * feat(mojaloop/#3818): added sequence diagram for FX timeout
    
    * feat(mojaloop/#3818): added transfer state diagrams
    
    * feat(mojaloop/#3818): added transfer state diagrams
    
    * feat(mojaloop/#3818): added transfer state diagrams
    
    * feat(mojaloop/#3818): added transfer state diagrams
    
    * feat(mojaloop/#3818): added transfer state diagrams
    
    * feat(mojaloop/#3818): finalize FX timeout diagrams
    
    * feat(mojaloop/#3818): added fxTimeout tables

--- seeds/transferState.js
+++ seeds/transferState.js
@@ -42,6 +42,11 @@ const transferStates = [
     description: 'The switch has reserved the transfer, and has been assigned to a settlement window.'
   },
   {
+    transferStateId: 'RECEIVED_FULFIL_DEPENDENT',
+    enumeration: 'RESERVED',
+    description: 'The switch has reserved the fxTransfer fulfilment.'
+  },
+  {
     transferStateId: 'COMMITTED',
     enumeration: 'COMMITTED',
     description: 'The switch has successfully performed the transfer.'
