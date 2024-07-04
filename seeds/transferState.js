62ca9a30dd04d7e68a9a03bb485211585402503f
Kevin Leyow <kleyow@gmail.com>
2024-07-04 13:41:13 -0500

    diff

--- seeds/transferState.js
+++ seeds/transferState.js
@@ -100,7 +100,12 @@ const transferStates = [
     transferStateId: 'SETTLED',
     enumeration: 'SETTLED',
     description: 'The switch has settled the transfer.'
-  }
+  },
+  {
+    transferStateId: 'RESERVED_FORWARDED',
+    enumeration: 'RESERVED_FORWARDED',
+    description: 'The switch has forwarded the transfer to a proxy participant'
+  },
 ]
 
 exports.seed = async function (knex) {
