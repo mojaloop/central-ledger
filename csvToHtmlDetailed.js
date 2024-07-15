9fd134375c0cae4a1c2ed7d8a9dfbbe79608188c
s-prak <sprakash_cse210557@mgit.ac.in>
2024-07-15 19:08:24 +0530

    updating html to look beautiful

--- csvToHtmlDetailed.js
+++ csvToHtmlDetailed.js
@@ -10,6 +10,66 @@ function csvToHtml(csvFile, htmlFile) {
         .on('end', () => {
             const headers = Object.keys(results[0]);
             let htmlContent ='<html>\n';
+            htmlContent += `<head>\n<style>
+                                /* CSS styles for the table */
+                                h1 {
+                                    font-family: Arial, sans-serif;
+                                    background-color: #f0f4f8;
+                                    color: #0073e6;
+                                    margin: 0;
+                                    padding: 0;
+                                    display: flex;
+                                    flex-direction: column;
+                                    align-items: center;
+                                    justify-content: center;
+                                }
+
+                                table {
+                                    width: 100%;
+                                    border-collapse: collapse;
+                                    margin-bottom: 20px;
+                                    font-size: 14px; /* Adjust font size */
+                                }
+
+                                table, th, td {
+                                    border: 1px solid #ddd;
+                                    padding: 6px; /* Reduce padding */
+                                    text-align: left;
+                                }
+
+                                th {
+                                    background-color: #f2f2f2;
+                                    color: #000;
+                                    background-color: #004080; /* Navy blue background for header */
+                                    color: #fff; /* White text for header */
+                                }
+
+                                td {
+                                    white-space: nowrap; /* Prevent wrapping by default */
+                                }
+
+                                td.name {
+                                    white-space: normal; /* Allow wrapping for 'Name' field */
+                                    max-width: 150px; /* Limit maximum width */
+                                }
+
+                                tr:nth-child(even) {
+                                    background-color: #f9f9f9;
+                                }
+
+                                tr:hover {
+                                    background-color: #e6f2ff; /* Light blue on hover */
+                                }
+
+                                caption {
+                                    font-size: 16px;
+                                    font-weight: bold;
+                                    margin-bottom: 10px;
+                                }
+
+                            </style>\n</head>\n`;
+            let htmlFileName = htmlFile.split('.');
+            htmlContent += `<h1>SBOM ${htmlFileName[0]}</h1>\n`;
             htmlContent += '<table border="1">\n';
             htmlContent += '  <thead>\n    <tr>\n';
             
@@ -31,7 +91,7 @@ function csvToHtml(csvFile, htmlFile) {
             
             fs.writeFile(htmlFile, htmlContent, (err) => {
                 if (err) throw err;
-                console.log('HTML table has been saved to ${htmlFile}');
+                console.log(`HTML table has been saved to ${htmlFile}`);
             });
         });
 }
--- csvToHtmlNormal.js
+++ csvToHtmlNormal.js
@@ -13,29 +13,64 @@ function csvToHtml(csvFile, htmlFile) {
             let htmlContent ='<html>\n';
             htmlContent += `<head>\n<style>
                                 /* CSS styles for the table */
+                                h1 {
+                                    font-family: Arial, sans-serif;
+                                    background-color: #f0f4f8;
+                                    color: #0073e6;
+                                    margin: 0;
+                                    padding: 0;
+                                    display: flex;
+                                    flex-direction: column;
+                                    align-items: center;
+                                    justify-content: center;
+                                }
+
                                 table {
                                     width: 100%;
                                     border-collapse: collapse;
                                     margin-bottom: 20px;
                                     font-size: 14px; /* Adjust font size */
                                 }
+
                                 table, th, td {
-                                    border: 1px solid black;
+                                    border: 1px solid #ddd;
                                     padding: 6px; /* Reduce padding */
                                     text-align: left;
                                 }
+
                                 th {
                                     background-color: #f2f2f2;
+                                    color: #000;
+                                    background-color: #004080; /* Navy blue background for header */
+                                    color: #fff; /* White text for header */
                                 }
-                                /* Wrap specific fields */
+
                                 td {
                                     white-space: nowrap; /* Prevent wrapping by default */
                                 }
+
                                 td.name {
                                     white-space: normal; /* Allow wrapping for 'Name' field */
                                     max-width: 150px; /* Limit maximum width */
                                 }
+
+                                tr:nth-child(even) {
+                                    background-color: #f9f9f9;
+                                }
+
+                                tr:hover {
+                                    background-color: #e6f2ff; /* Light blue on hover */
+                                }
+
+                                caption {
+                                    font-size: 16px;
+                                    font-weight: bold;
+                                    margin-bottom: 10px;
+                                }
+
                             </style>\n</head>\n`;
+            let htmlFileName = htmlFile.split('.');
+            htmlContent += `<h1>SBOM ${htmlFileName[0]}</h1>\n`;
             htmlContent += '<table border="1">\n';
             htmlContent += '  <thead>\n    <tr>\n';
             
@@ -61,7 +96,7 @@ function csvToHtml(csvFile, htmlFile) {
             
             fs.writeFile(htmlFile, htmlContent, (err) => {
                 if (err) throw err;
-                console.log('HTML table has been saved to ${htmlFile}');
+                console.log(`HTML table has been saved to ${htmlFile}`);
             });
         });
 }
