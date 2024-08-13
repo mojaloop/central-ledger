988e23fff42f7346dd983cc77ac1486c8d3f7638
Kevin Leyow <kleyow@gmail.com>
2024-08-13 17:35:03 -0500

    changes

---
+++ migrations/950117_fxQuoteResponseConversionTerms.js
@@ -0,0 +1,37 @@
+'use strict'
+
+exports.up = (knex) => {
+  return knex.schema.hasTable('fxQuoteResponseConversionTerms').then((exists) => {
+    if (!exists) {
+      return knex.schema.createTable('fxQuoteResponseConversionTerms', (t) => {
+        t.string('conversionId').primary().notNullable()
+
+        // reference to the original fxQuote
+        t.string('conversionRequestId', 36).notNullable()
+        t.foreign('conversionRequestId').references('conversionRequestId').inTable('fxQuote')
+
+        // reference to the original fxQuoteResponse
+        t.bigIncrements('fxQuoteResponseId', 36).notNullable()
+        t.foreign('fxQuoteResponseId').references('fxQuoteResponseId').inTable('fxQuoteResponse')
+
+        t.integer('amountTypeId').unsigned().notNullable().comment('This is part of the transaction type that contains valid elements for - Amount Type')
+        t.foreign('amountTypeId').references('amountTypeId').inTable('amountType')
+        t.string('initiatingFsp', 255)
+        t.string('counterPartyFsp', 255)
+        t.decimal('sourceAmount', 18, 4).notNullable()
+        t.string('sourceCurrency', 3).notNullable()
+        t.foreign('sourceCurrency').references('currencyId').inTable('currency')
+        t.decimal('targetAmount', 18, 4).notNullable()
+        t.string('targetCurrency', 3).notNullable()
+        t.foreign('targetCurrency').references('currencyId').inTable('currency')
+
+        // time keeping
+        t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable().comment('System dateTime stamp pertaining to the inserted record')
+      })
+    }
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('fxQuoteResponseConversionTerms')
+}
--- migrations/950119_fxCharge.js
+++ migrations/950119_fxCharge.js
@@ -8,11 +8,9 @@ exports.up = (knex) => {
         t.string('chargeType', 32).notNullable().comment('A description of the charge which is being levied.')
 
         // fxCharge should only be sent back in the response to an fxQuote
-        // but lets reference the original fxQuote in the charge table
-        t.string('conversionRequestId', 36).notNullable()
-        t.foreign('conversionRequestId').references('conversionRequestId').inTable('fxQuote')
-        t.bigInteger('fxQuoteResponseId').unsigned().defaultTo(null).nullable().comment('The response to the initial fxQuote')
-        t.foreign('fxQuoteResponseId').references('fxQuoteResponseId').inTable('fxQuoteResponse')
+        // so reference the terms in fxQuoteResponse `conversionTerms`
+        t.string('conversionId', 36).notNullable()
+        t.foreign('conversionId').references('conversionId').inTable('fxQuoteResponseConversionTerms')
 
         t.decimal('sourceAmount', 18, 4).nullable().comment('The amount of the charge which is being levied, expressed in the source currency.')
         t.string('sourceCurrency', 3).nullable().comment('The currency in which the source amount charge is being levied.')
