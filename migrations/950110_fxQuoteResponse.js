976297f924c4cd1513ffd0e212d227afa68f10a5
Kevin Leyow <kleyow@gmail.com>
2024-08-14 00:58:35 -0500

    remove

--- migrations/950110_fxQuoteResponse.js
+++ migrations/950110_fxQuoteResponse.js
@@ -13,19 +13,6 @@ exports.up = (knex) => {
         // ilpCondition sent in FXP response
         t.string('ilpCondition', 256).notNullable()
 
-        // break out conversionTerms into separate table?
-        t.string('conversionId', 36).notNullable()
-        t.integer('amountTypeId').unsigned().notNullable().comment('This is part of the transaction type that contains valid elements for - Amount Type')
-        t.foreign('amountTypeId').references('amountTypeId').inTable('amountType')
-        t.string('initiatingFsp', 255)
-        t.string('counterPartyFsp', 255)
-        t.decimal('sourceAmount', 18, 4).notNullable()
-        t.string('sourceCurrency', 3).notNullable()
-        t.foreign('sourceCurrency').references('currencyId').inTable('currency')
-        t.decimal('targetAmount', 18, 4).notNullable()
-        t.string('targetCurrency', 3).notNullable()
-        t.foreign('targetCurrency').references('currencyId').inTable('currency')
-
         // time keeping
         t.dateTime('expirationDate').defaultTo(null).nullable().comment('Optional expiration for the requested transaction')
         t.dateTime('createdDate').defaultTo(knex.fn.now()).notNullable().comment('System dateTime stamp pertaining to the inserted record')
