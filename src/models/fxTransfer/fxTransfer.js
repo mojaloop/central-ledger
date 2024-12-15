4379bbf079f32266882e0a26de0a9b6dc839a9b6
Kevin Leyow <kleyow@gmail.com>
2024-12-15 15:49:02 -0500

    chore: update licenses

--- src/models/fxTransfer/fxTransfer.js
+++ src/models/fxTransfer/fxTransfer.js
@@ -1,3 +1,38 @@
+/*****
+ License
+ --------------
+ Copyright © 2020-2024 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Mojaloop Foundation for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Mojaloop Foundation
+ - Name Surname <name.surname@mojaloop.io>
+
+ * Infitx
+ - Vijay Kumar Guthi <vijaya.guthi@infitx.com>
+ - Kevin Leyow <kevin.leyow@infitx.com>
+ - Kalin Krustev <kalin.krustev@infitx.com>
+ - Steven Oderayi <steven.oderayi@infitx.com>
+ - Eugen Klymniuk <eugen.klymniuk@infitx.com>
+
+ --------------
+
+ ******/
+
 const Metrics = require('@mojaloop/central-services-metrics')
 const ErrorHandler = require('@mojaloop/central-services-error-handling')
 const { Enum, Util } = require('@mojaloop/central-services-shared')
