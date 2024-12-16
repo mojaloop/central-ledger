6a1d4d2469e2a86ee96b369cd6996b553d321390
Devarsh <devarshshah2608@gmail.com>
2024-12-16 12:19:20 +0000

    replace deprecated @hapi/joi with joi

--- src/api/ledgerAccountTypes/routes.js
+++ src/api/ledgerAccountTypes/routes.js
@@ -25,7 +25,9 @@
 'use strict'
 
 const Handler = require('./handler')
-const Joi = require('@hapi/joi')
+// const Joi = require('@hapi/joi')
+const Joi = require('joi');
+
 const tags = ['api', 'ledgerAccountTypes']
 
 module.exports = [
