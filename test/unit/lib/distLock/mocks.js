e17ab9157c396cef59c0d51a161db1eee971f8a9
Steven Oderayi <oderayi@gmail.com>
2025-06-23 16:05:00 +0100

    test: add mocks.js

---
+++ test/unit/lib/distLock/mocks.js
@@ -0,0 +1,32 @@
+const sinon = require('sinon')
+
+const mockRedis = sinon.stub()
+const mockCluster = sinon.stub()
+mockRedis.Cluster = mockCluster
+const mockRedlock = sinon.stub().returns({
+  acquire: async () => ({ value: 'test-lock-value' }),
+  release: async () => {},
+  extend: async () => ({ value: 'test-lock-extend-value' }),
+  on: () => {}
+})
+
+const mockLogger = {
+  debug: sinon.stub(),
+  error: sinon.stub()
+}
+
+const mockConfig = {
+  redisConfigs: [
+    {
+      type: 'redis',
+      host: 'localhost',
+      port: 6379
+    }
+  ],
+  driftFactor: 0.01,
+  retryCount: 3,
+  retryDelay: 200,
+  retryJitter: 100
+}
+
+module.exports = { mockRedis, mockCluster, mockConfig, mockLogger, mockRedlock }
