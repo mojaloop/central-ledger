565c46b8ae3fe4a97c6055a90f7ad0e0d0ec6b95
Lewis Daly <lewis@tigerbeetle.com>
2026-02-25 16:47:14 +0100

    feat: change from glob npm module to native glob

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const Glob = require('glob')
+const { globSync } = require('node:fs')
 
 exports.plugin = {
   name: 'api routes',
   register: function (server) {
-    Glob.sync('**/routes.js', { cwd: __dirname, ignore: 'routes.js' })
+    globSync('**/routes.js', { cwd: __dirname })
+      .filter(x => x !== 'routes.js')
       .forEach(x => server.route(require('./' + x)))
   }
 }
